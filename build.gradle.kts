47471baad7c6ecc735ae3050da37a0b6467b701a
Salomon BRYS <salomon@kodein.net>
2020-06-19 16:08:00 +0200

    Root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,179 @@
-buildscript {
-    val kotlin_version: String by extra
-    val gradle_android_version: String by extra
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask
 
-    repositories {
-        maven("https://kotlin.bintray.com/kotlinx")
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+plugins {
+    application
+    kotlin("multiplatform") version "1.4-M2-mt"
+    kotlin("plugin.serialization") version "1.4-M2-mt"
+}
 
-        google()
-        jcenter()
+group = "fr.acinq.eklair"
+version = "1.0"
+
+application {
+    mainClassName = "fr.acinq.eklair.Boot"
+}
+
+repositories {
+    mavenLocal()
+    maven("https://dl.bintray.com/kotlin/kotlinx")
+    maven("https://dl.bintray.com/kotlin/ktor")
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    maven("https://dl.bintray.com/kodein-framework/kodein-dev")
+    google()
+    jcenter()
+}
+
+val currentOs = org.gradle.internal.os.OperatingSystem.current()
+
+kotlin {
+    jvm {
+        val main by compilations.getting {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
     }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
-        classpath("com.android.tools.build:gradle:$gradle_android_version")
 
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+    val isWinHost = System.getProperty("os.name").startsWith("Windows", ignoreCase = true)
+    linuxX64("linux")
+
+    if (currentOs.isMacOsX) {
+        ios {
+            binaries {
+                framework()
+            }
+        }
+    }
+
+    sourceSets {
+        val ktor_version = "1.3.2-1.4-M2"
+
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("fr.acinq:bitcoink:0.1-1.4-M2")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7-native-mt-1.4-M2")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0-1.4-M2")
+                implementation("org.kodein.log:kodein-log:0.2.0-1.4-M2-dev-20")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+                implementation("io.ktor:ktor-client-core:$ktor_version")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("fr.acinq.bitcoin:secp256k1-jni:1.3")
+                implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+                implementation("io.ktor:ktor-network:$ktor_version")
+                implementation("org.slf4j:slf4j-api:1.7.29")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("org.bouncycastle:bcprov-jdk15on:1.64")
+            }
+        }
+        val nativeMain by creating {
+            dependsOn(commonMain)
+        }
+        val nativeTest by creating {
+            dependsOn(commonTest)
+        }
+        if (!isWinHost) {
+            val linuxMain by getting {
+                dependsOn(nativeMain)
+            }
+            val linuxTest by getting {
+                dependsOn(nativeTest)
+                dependencies {
+                    implementation("io.ktor:ktor-client-curl:$ktor_version")
+                }
+            }
+        }
+
+        if (currentOs.isMacOsX) {
+            val iosMain by getting {
+                dependsOn(nativeMain)
+                dependencies {
+                    implementation(kotlin("stdlib"))
+                }
+            }
+            val iosTest by getting {
+                dependsOn(nativeTest)
+                dependencies {
+                    implementation("io.ktor:ktor-client-ios:$ktor_version")
+                }
+            }
+        }
+    }
+
+    sourceSets.all {
+        languageSettings.useExperimentalAnnotation("kotlin.ExperimentalStdlibApi")
+    }
+
+    // Create a task building a fat framework.
+    tasks.create("createFatFramework", FatFrameworkTask::class) {
+        val buildType: String = project.findProperty("kotlin.build.type")?.toString() ?: "DEBUG"
+
+        // The fat framework must have the same base name as the initial frameworks.
+        baseName = "eklair"
+
+        // The default destination directory is '<build directory>/fat-framework'.
+        destinationDir = buildDir.resolve("eklair/${buildType.toLowerCase()}")
+
+        val iosTargets = listOf(targets.findByName("iosArm64") as? KotlinNativeTarget, targets.findByName("iosX64") as? KotlinNativeTarget)
+        // Specify the frameworks to be merged.
+        val frameworksBinaries = iosTargets.mapNotNull { it?.binaries?.getFramework(buildType) }
+        from(frameworksBinaries)
+        dependsOn(frameworksBinaries.map { it.linkTask })
+
+        // disable gradle's up to date checking
+        outputs.upToDateWhen { false }
+
+        doLast {
+            val srcFile: File = destinationDir
+            val targetDir = System.getProperty("configuration.build.dir") ?: project.buildDir.path
+            println("\uD83C\uDF4E Copying ${srcFile} to ${targetDir}")
+            copy {
+                from(srcFile)
+                into(targetDir)
+                include("*.framework/**")
+                include("*.framework.dSYM/**")
+            }
+        }
     }
 }
 
-allprojects {
-    repositories {
-        mavenLocal()
-        maven("https://dl.bintray.com/kotlin/kotlinx")
-        maven("https://dl.bintray.com/kotlin/ktor")
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
-        maven("https://dl.bintray.com/kodein-framework/kodein-dev")
-        google()
-        jcenter()
-    }
-}
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+// Disable cross compilation
+afterEvaluate {
+    val currentOs = org.gradle.internal.os.OperatingSystem.current()
+    val targets = when {
+        currentOs.isLinux -> listOf("mingwX64", "macosX64")
+        currentOs.isMacOsX -> listOf("mingwX64", "linuxX64")
+        currentOs.isWindows -> listOf("linuxX64", "macosX64")
+        else -> listOf("mingwX64", "linuxX64", "macosX64")
+    }.mapNotNull { kotlin.targets.findByName(it) as? org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget }
+
+    configure(targets) {
+        compilations.all {
+            cinterops.all { tasks[interopProcessingTaskName].enabled = false }
+            compileKotlinTask.enabled = false
+            tasks[processResourcesTaskName].enabled = false
+        }
+        binaries.all { linkTask.enabled = false }
+
+        mavenPublication {
+            val publicationToDisable = this
+            tasks.withType<AbstractPublishToMaven>().all { onlyIf { publication != publicationToDisable } }
+            tasks.withType<GenerateModuleMetadata>().all { onlyIf { publication.get() != publicationToDisable } }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name="eklair"
 
-include(":common")
-
-val include_android: String by extra
-if (include_android == "true") {
-    println("Include Android")
-    include(":phoenix-android")
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven ("https://dl.bintray.com/kotlin/kotlin-eap")
+        jcenter()
+    }
 }
