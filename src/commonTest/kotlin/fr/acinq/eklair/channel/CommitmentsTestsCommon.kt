da223feeb562d746e4ccf3a47abeadfec843a800
sstone <fabrice.drouin@acinq.fr>
2020-06-22 11:58:47 +0200

    Add last Commitments test

--- src/commonTest/kotlin/fr/acinq/eklair/channel/CommitmentsTestsCommon.kt
+++ src/commonTest/kotlin/fr/acinq/eklair/channel/CommitmentsTestsCommon.kt
@@ -287,6 +287,156 @@ class CommitmentsTests {
         assertEquals(ac8.availableBalanceForReceive(), b)
     }
 
+    @Test
+    fun `correct values for availableForSend - availableForReceive (multiple htlcs)`() {
+        val (alice, bob) = reachNormal()
+
+        val fee = 1720000.msat // fee due to the additional htlc output
+        val funderFeeReserve = fee * 2 // extra reserve to handle future fee increase
+        val a = (772760000.msat) - fee - funderFeeReserve // initial balance alice
+        val b = 190000000.msat // initial balance bob
+        val p1 = 10000000.msat // a->b payment
+        val p2 = 20000000.msat // a->b payment
+        val p3 = 40000000.msat // b->a payment
+        val ac0 = alice.commitments
+        val bc0 = bob.commitments
+
+        assertTrue(ac0.availableBalanceForSend() > p1 + p2) // alice can afford the payment
+        assertTrue(bc0.availableBalanceForSend() > p3) // alice can afford the payment
+        assertEquals(ac0.availableBalanceForSend(), a)
+        assertEquals(ac0.availableBalanceForReceive(), b)
+        assertEquals(bc0.availableBalanceForSend(), b)
+        assertEquals(bc0.availableBalanceForReceive(), a)
+
+        val currentBlockHeight = 144L
+
+        val (payment_preimage1, cmdAdd1) = TestsHelper.makeCmdAdd(p1, bob.staticParams.nodeParams.nodeId, currentBlockHeight)
+        val (ac1, add1) = (ac0.sendAdd(cmdAdd1, Origin.Local(UUID.randomUUID()), currentBlockHeight) as Try.Success<Pair<Commitments, UpdateAddHtlc>>).result
+        assertEquals(ac1.availableBalanceForSend(), a - p1 - fee) // as soon as htlc is sent, alice sees its balance decrease (more than the payment amount because of the commitment fees)
+        assertEquals(ac1.availableBalanceForReceive(), b)
+
+        val (payment_preimage2, cmdAdd2) = TestsHelper.makeCmdAdd(p2, bob.staticParams.nodeParams.nodeId, currentBlockHeight)
+        val (ac2, add2) = (ac1.sendAdd(cmdAdd2, Origin.Local(UUID.randomUUID()), currentBlockHeight) as Try.Success<Pair<Commitments, UpdateAddHtlc>>).result
+        assertEquals(ac2.availableBalanceForSend(), a - p1 - fee - p2 - fee) // as soon as htlc is sent, alice sees its balance decrease (more than the payment amount because of the commitment fees)
+        assertEquals(ac2.availableBalanceForReceive(), b)
+
+        val (payment_preimage3, cmdAdd3) = TestsHelper.makeCmdAdd(p3, alice.staticParams.nodeParams.nodeId, currentBlockHeight)
+        val (bc1, add3) = (bc0.sendAdd(cmdAdd3, Origin.Local(UUID.randomUUID()), currentBlockHeight) as Try.Success<Pair<Commitments, UpdateAddHtlc>>).result
+        assertEquals(bc1.availableBalanceForSend(), b - p3) // as soon as htlc is sent, alice sees its balance decrease (more than the payment amount because of the commitment fees)
+        assertEquals(bc1.availableBalanceForReceive(), a)
+
+        val bc2 = (bc1.receiveAdd(add1) as Try.Success<Commitments>).result
+        assertEquals(bc2.availableBalanceForSend(), b - p3)
+        assertEquals(bc2.availableBalanceForReceive(), a - p1 - fee)
+
+        val bc3 = (bc2.receiveAdd(add2) as Try.Success<Commitments>).result
+        assertEquals(bc3.availableBalanceForSend(), b - p3)
+        assertEquals(bc3.availableBalanceForReceive(), a - p1 - fee - p2 - fee)
+
+        val ac3 = (ac2.receiveAdd(add3) as Try.Success<Commitments>).result
+        assertEquals(ac3.availableBalanceForSend(), a - p1 - fee - p2 - fee)
+        assertEquals(ac3.availableBalanceForReceive(), b - p3)
+
+        val (ac4, commit1) = (ac3.sendCommit(alice.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, CommitSig>>).result
+        assertEquals(ac4.availableBalanceForSend(), a - p1 - fee - p2 - fee)
+        assertEquals(ac4.availableBalanceForReceive(), b - p3)
+
+        val (bc4, revocation1) = (bc3.receiveCommit(commit1, bob.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, RevokeAndAck>>).result
+        assertEquals(bc4.availableBalanceForSend(), b - p3)
+        assertEquals(bc4.availableBalanceForReceive(), a - p1 - fee - p2 - fee)
+
+        val ac5 = (ac4.receiveRevocation(revocation1) as Try.Success<Commitments>).result
+        assertEquals(ac5.availableBalanceForSend(), a - p1 - fee - p2 - fee)
+        assertEquals(ac5.availableBalanceForReceive(), b - p3)
+
+        val (bc5, commit2) = (bc4.sendCommit(bob.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, CommitSig>>).result
+        assertEquals(bc5.availableBalanceForSend(), b - p3)
+        assertEquals(bc5.availableBalanceForReceive(), a - p1 - fee - p2 - fee)
+
+        val (ac6, revocation2) = (ac5.receiveCommit(commit2, alice.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, RevokeAndAck>>).result
+        assertEquals(ac6.availableBalanceForSend(), a - p1 - fee - p2 - fee - fee ) // alice has acknowledged b's hltc so it needs to pay the fee for it
+        assertEquals(ac6.availableBalanceForReceive(), b - p3)
+
+        val bc6 = (bc5.receiveRevocation(revocation2) as Try.Success<Commitments>).result
+        assertEquals(bc6.availableBalanceForSend(), b - p3)
+        assertEquals(bc6.availableBalanceForReceive(), a - p1 - fee - p2 - fee - fee)
+
+        val (ac7, commit3) = (ac6.sendCommit(alice.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, CommitSig>>).result
+        assertEquals(ac7.availableBalanceForSend(), a - p1 - fee - p2 - fee - fee)
+        assertEquals(ac7.availableBalanceForReceive(), b - p3)
+
+        val (bc7, revocation3) = (bc6.receiveCommit(commit3, bob.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, RevokeAndAck>>).result
+        assertEquals(bc7.availableBalanceForSend(), b - p3)
+        assertEquals(bc7.availableBalanceForReceive(), a - p1 - fee - p2 - fee - fee)
+
+        val ac8 = (ac7.receiveRevocation(revocation3) as Try.Success<Commitments>).result
+        assertEquals(ac8.availableBalanceForSend(), a - p1 - fee - p2 - fee - fee)
+        assertEquals(ac8.availableBalanceForReceive(), b - p3)
+
+        val cmdFulfill1 = CMD_FULFILL_HTLC(0, payment_preimage1)
+        val (bc8, fulfill1) = (bc7.sendFulfill(cmdFulfill1) as Try.Success<Pair<Commitments, UpdateFulfillHtlc>>).result
+        assertEquals(bc8.availableBalanceForSend(), b + p1 - p3) // as soon as we have the fulfill, the balance increases
+        assertEquals(bc8.availableBalanceForReceive(), a - p1 - fee - p2 - fee - fee)
+
+        val cmdFail2 = CMD_FAIL_HTLC(1, CMD_FAIL_HTLC.Reason.Failure(IncorrectOrUnknownPaymentDetails(p2, 42)))
+        val (bc9, fail2) = (bc8.sendFail(cmdFail2, bob.staticParams.nodeParams.privateKey) as Try.Success<Pair<Commitments, UpdateFailHtlc>>).result
+        assertEquals(bc9.availableBalanceForSend(), b + p1 - p3)
+        assertEquals(bc9.availableBalanceForReceive(), a - p1 - fee - p2 - fee - fee) // a's balance won't return to previous before she acknowledges the fail
+
+        val cmdFulfill3 = CMD_FULFILL_HTLC(0, payment_preimage3)
+        val (ac9, fulfill3) = (ac8.sendFulfill(cmdFulfill3) as Try.Success<Pair<Commitments, UpdateFulfillHtlc>>).result
+        assertEquals(ac9.availableBalanceForSend(), a - p1 - fee - p2 - fee + p3)
+        assertEquals(ac9.availableBalanceForReceive(), b - p3)
+
+        val ac10 = (ac9.receiveFulfill(fulfill1) as Try.Success<Triple<Commitments, Origin, UpdateAddHtlc>>).result.first
+        assertEquals(ac10.availableBalanceForSend(), a - p1 - fee - p2 - fee + p3)
+        assertEquals(ac10.availableBalanceForReceive(), b + p1 - p3)
+
+        val ac11 = (ac10.receiveFail(fail2) as Try.Success<Triple<Commitments, Origin, UpdateAddHtlc>>).result.first
+        assertEquals(ac11.availableBalanceForSend(), a - p1 - fee - p2 - fee + p3)
+        assertEquals(ac11.availableBalanceForReceive(), b + p1 - p3)
+
+        val bc10 = (bc9.receiveFulfill(fulfill3) as Try.Success<Triple<Commitments, Origin, UpdateAddHtlc>>).result.first
+        assertEquals(bc10.availableBalanceForSend(), b + p1 - p3)
+        assertEquals(bc10.availableBalanceForReceive(), a - p1 - fee - p2 - fee + p3) // the fee for p3 disappears
+
+        val (ac12, commit4) = (ac11.sendCommit(alice.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, CommitSig>>).result
+        assertEquals(ac12.availableBalanceForSend(), a - p1 - fee - p2 - fee + p3)
+        assertEquals(ac12.availableBalanceForReceive(), b + p1 - p3)
+
+        val (bc11, revocation4) = (bc10.receiveCommit(commit4, bob.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, RevokeAndAck>>).result
+        assertEquals(bc11.availableBalanceForSend(), b + p1 - p3)
+        assertEquals(bc11.availableBalanceForReceive(), a - p1 - fee - p2 - fee + p3)
+
+        val ac13 = (ac12.receiveRevocation(revocation4) as Try.Success<Commitments>).result
+        assertEquals(ac13.availableBalanceForSend(), a - p1 - fee - p2 - fee +p3)
+        assertEquals(ac13.availableBalanceForReceive(), b + p1 - p3)
+
+        val (bc12, commit5) = (bc11.sendCommit(bob.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, CommitSig>>).result
+        assertEquals(bc12.availableBalanceForSend(), b + p1 - p3)
+        assertEquals(bc12.availableBalanceForReceive(), a - p1 - fee - p2 - fee + p3)
+
+        val (ac14, revocation5) = (ac13.receiveCommit(commit5, alice.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, RevokeAndAck>>).result
+        assertEquals(ac14.availableBalanceForSend(), a - p1 + p3)
+        assertEquals(ac14.availableBalanceForReceive(), b + p1 - p3)
+
+        val bc13 = (bc12.receiveRevocation(revocation5) as Try.Success<Commitments>).result
+        assertEquals(bc13.availableBalanceForSend(), b + p1 - p3)
+        assertEquals(bc13.availableBalanceForReceive(), a - p1 + p3)
+
+        val (ac15, commit6) = (ac14.sendCommit(alice.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, CommitSig>>).result
+        assertEquals(ac15.availableBalanceForSend(), a - p1 + p3)
+        assertEquals(ac15.availableBalanceForReceive(), b + p1 - p3)
+
+        val (bc14, revocation6) = (bc13.receiveCommit(commit6, bob.staticParams.nodeParams.keyManager, logger) as Try.Success<Pair<Commitments, RevokeAndAck>>).result
+        assertEquals(bc14.availableBalanceForSend(), b + p1 - p3)
+        assertEquals(bc14.availableBalanceForReceive(), a - p1 + p3)
+
+        val ac16 = (ac15.receiveRevocation(revocation6) as Try.Success<Commitments>).result
+        assertEquals(ac16.availableBalanceForSend(), a - p1 + p3)
+        assertEquals(ac16.availableBalanceForReceive(), b + p1 - p3)
+    }
+
     // See https://github.com/lightningnetwork/lightning-rfc/issues/728
     @Test 
     fun`funder keeps additional reserve to avoid channel being stuck`() {
