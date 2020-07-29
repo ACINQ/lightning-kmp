e5d7df9757b3a67a99290a788c8b19ef8fd2f4d6
Romain Boisselle <romain@boisselle.dev>
2020-07-29 17:24:31 +0200

    native runTest

---
+++ src/iosTest/kotlin/fr/acinq/eklair/utils/runTestIos.kt
@@ -0,0 +1,20 @@
+package fr.acinq.eklair.utils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import platform.Foundation.NSDate
+import platform.Foundation.NSRunLoop
+import platform.Foundation.dateWithTimeIntervalSinceNow
+import platform.Foundation.runUntilDate
+
+actual fun runTest(block: suspend CoroutineScope.() -> Unit) {
+    var result: Result<Unit>? = null
+
+    IosMainScope().launch {
+        result = kotlin.runCatching { block() }
+    }
+
+    while (result == null) NSRunLoop.mainRunLoop.runUntilDate(NSDate.dateWithTimeIntervalSinceNow(0.01))
+
+    result!!.getOrThrow()
+}
