e175ccd9eb227336badc01f819c4e70fb793ecc9
Romain Boisselle <romain@boisselle.dev>
2020-07-31 18:32:54 +0200

    throw ConnectionClosed exception on JvmTcpSocket.receiveAvailable

--- src/commonTest/kotlin/fr/acinq/eklair/io/TcpSocketTest.kt
+++ src/commonTest/kotlin/fr/acinq/eklair/io/TcpSocketTest.kt
@@ -7,14 +7,17 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class TcpSocketTest {
+
+    private val serverVersionRpc = buildString {
+        append("""{ "id":"0", "method":"server.version", "params": ["3.3.6", "1.4"]}""")
+        appendLine()
+    }.toByteArray()
+
     @Test
     fun `TCP connection`() = runTest {
         withTimeout(5_000) {
             val socket = TcpSocket.Builder().connect("localhost", 51001, false)
-            socket.send(buildString {
-                append("""{ "id":"0", "method":"server.version", "params": ["3.3.6", "1.4"]}""")
-                appendLine()
-            }.toByteArray())
+            socket.send(serverVersionRpc)
             val size = socket.receiveAvailable(ByteArray(32))
             assertTrue { size > 0 }
             socket.close()
@@ -25,10 +28,7 @@ class TcpSocketTest {
     fun `SSL connection`() = runTest {
         withTimeout(5_000) {
             val socket = TcpSocket.Builder().connect("electrum.acinq.co", 50002, true)
-            socket.send(buildString {
-                append("""{ "id":"0", "method":"server.version", "params": ["3.3.6", "1.4"]}""")
-                appendLine()
-            }.toByteArray())
+            socket.send(serverVersionRpc)
             val size = socket.receiveAvailable(ByteArray(32))
             assertTrue { size > 0 }
             socket.close()
