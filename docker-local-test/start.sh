8841859180023429f19286f0f271119cf61298c7
Salomon BRYS <salomon@kodein.net>
2020-08-15 10:44:32 +0200

    No need to have an external IP address to start docker

--- docker-local-test/start.sh
+++ docker-local-test/start.sh
@@ -8,15 +8,14 @@ sleep 2
 
 docker build -t eclair-gui eclair-gui
 
-ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-xhost + $ip
+xhost + 127.0.0.1
 
 docker run --rm \
     --name eclair \
     --net eklair-net \
-    -e DISPLAY=$ip:0 \
+    -e DISPLAY=host.docker.internal:0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -p 48001:48001 \
-    eclair-gui
+    -it eclair-gui
 
 ../docker-cleanup.sh
