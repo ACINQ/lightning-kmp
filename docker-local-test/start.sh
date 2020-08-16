0cfa91c7d637d503bf955c64d5aca46a92079a8c
Salomon BRYS <salomon@kodein.net>
2020-08-16 12:54:12 +0200

    Linux docker GUI support

--- docker-local-test/start.sh
+++ docker-local-test/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ "$(uname)" = "Linux" ]; then
+    dockDisplay=unix:0
+    diplayHost=local:root
+elif [ "$(uname)" = "Darwin" ]; then
+    dockDisplay=host.docker.internal:0
+    diplayHost=127.0.0.1
+else
+    echo "Unsupported OS: $(uname)"
+    exit 1
+fi
+
 ../docker-env.sh
 
 sleep 2
@@ -8,14 +19,16 @@ sleep 2
 
 docker build -t eclair-gui eclair-gui
 
-xhost + 127.0.0.1
+xhost + $diplayHost
 
 docker run --rm \
     --name eclair \
     --net eklair-net \
-    -e DISPLAY=host.docker.internal:0 \
+    -e DISPLAY=$dockDisplay \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -p 48001:48001 \
     -it eclair-gui
+
+xhost - $diplayHost
 
 ../docker-cleanup.sh
