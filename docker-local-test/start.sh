7b71af220b1e63cfe7448bc565dca45f86a56d8c
Salomon BRYS <salomon@kodein.net>
2020-08-17 10:59:17 +0200

    Docker with user

--- docker-local-test/start.sh
+++ docker-local-test/start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 if [ "$(uname)" = "Linux" ]; then
-    dockDisplay=unix:0
-    diplayHost=local:root
+    displayAddr=unix$DISPLAY
 elif [ "$(uname)" = "Darwin" ]; then
-    dockDisplay=host.docker.internal:0
-    diplayHost=127.0.0.1
+    displayAddr=host.docker.internal:0
+    xhost + 127.0.0.1
 else
     echo "Unsupported OS: $(uname)"
     exit 1
@@ -17,18 +16,18 @@ sleep 2
 
 ./genblocks.sh 150
 
-docker build -t eclair-gui eclair-gui
-
-xhost + $diplayHost
+docker build \
+    --build-arg user=$USER \
+    --build-arg uid=$(id -u) \
+    --build-arg gid=$(id -g) \
+    -t eclair-gui eclair-gui
 
 docker run --rm \
     --name eclair \
     --net eklair-net \
-    -e DISPLAY=$dockDisplay \
+    -e DISPLAY=$displayAddr \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -p 48001:48001 \
     -it eclair-gui
-
-xhost - $diplayHost
 
 ../docker-cleanup.sh
