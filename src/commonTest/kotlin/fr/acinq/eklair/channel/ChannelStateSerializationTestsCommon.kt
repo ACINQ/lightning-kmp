0249ee0d2fa6dd93311123ade3da6459e1470a72
Salomon BRYS <salomon@kodein.net>
2020-09-02 16:19:00 +0200

    Kotlin/Native does not seem to support serialization of polymorphic root.

--- src/commonTest/kotlin/fr/acinq/eklair/channel/ChannelStateSerializationTestsCommon.kt
+++ src/commonTest/kotlin/fr/acinq/eklair/channel/ChannelStateSerializationTestsCommon.kt
@@ -4,12 +4,13 @@ import fr.acinq.eklair.blockchain.fee.ConstantFeeEstimator
 import fr.acinq.eklair.crypto.KeyManager
 import fr.acinq.eklair.wire.Tlv
 import fr.acinq.eklair.wire.UpdateMessage
+import kotlinx.serialization.*
 import kotlinx.serialization.cbor.Cbor
-import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.modules.SerializersModule
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@OptIn(ExperimentalSerializationApi::class)
 class ChannelStateSerializationTestsCommon {
     val serializationModules = SerializersModule {
         include(Tlv.serializationModule)
@@ -21,15 +22,19 @@ class ChannelStateSerializationTestsCommon {
     val cbor = Cbor {
         serializersModule = serializationModules
     }
+
+    @Serializable
+    data class ChannelStateHolder(val state: ChannelState)
+
     @Test
     fun `serialize normal state`() {
         val (alice, bob) = TestsHelper.reachNormal()
-        val bytes = cbor.encodeToByteArray(ChannelState.serializer(), alice)
-        val check = cbor.decodeFromByteArray<ChannelState>(bytes)
+        val bytes = cbor.encodeToByteArray(ChannelStateHolder(alice))
+        val check = cbor.decodeFromByteArray<ChannelStateHolder>(bytes).state
         assertEquals(alice,  check)
 
-        val bytes1 = cbor.encodeToByteArray(ChannelState.serializer(), bob)
-        val check1 = cbor.decodeFromByteArray<ChannelState>(bytes1)
+        val bytes1 = cbor.encodeToByteArray(ChannelStateHolder(bob))
+        val check1 = cbor.decodeFromByteArray<ChannelStateHolder>(bytes1).state
         assertEquals(bob,  check1)
     }
 }
