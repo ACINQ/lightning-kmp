1edffee5b3cff26f4244073e34adc69330826d23
Romain Boisselle <romain@boisselle.dev>
2020-12-02 06:55:23 +0100

    implement InMemoryChannelsDb

--- src/commonMain/kotlin/fr/acinq/eclair/db/ChannelsDb.kt
+++ src/commonMain/kotlin/fr/acinq/eclair/db/ChannelsDb.kt
@@ -10,10 +10,4 @@ interface ChannelsDb {
     suspend fun removeChannel(channelId: ByteVector32)
 
     suspend fun listLocalChannels(): List<ChannelStateWithCommitments>
-
-    suspend fun addHtlcInfo(channelId: ByteVector32, commitmentNumber: Long, paymentHash: ByteVector32, cltvExpiry: CltvExpiry)
-
-    suspend fun listHtlcInfos(channelId: ByteVector32, commitmentNumber: Long): List<Pair<ByteVector32, CltvExpiry>>
-
-    fun close()
 }
