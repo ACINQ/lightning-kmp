364b6771445fc591ff4be7493fbcc849024dce13
Romain Boisselle <romain@boisselle.dev>
2020-12-07 14:27:05 +0100

    fix failing tests

--- src/commonMain/kotlin/fr/acinq/eclair/db/Databases.kt
+++ src/commonMain/kotlin/fr/acinq/eclair/db/Databases.kt
@@ -10,6 +10,6 @@ interface Databases {
  * This should only be used in tests: applications should inject their own implementations depending on the database backend available on the platform.
  */
 data class InMemoryDatabases(
-    override val channels: InMemoryChannelsDb = InMemoryChannelsDb(),
-    override val payments: InMemoryPaymentsDb = InMemoryPaymentsDb()
+    override val channels: InMemoryChannelsDb,
+    override val payments: InMemoryPaymentsDb
 ) : Databases
