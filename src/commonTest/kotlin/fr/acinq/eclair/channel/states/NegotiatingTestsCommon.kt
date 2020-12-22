1d00a178528900c46421e1260813b997a2b79913
sstone <fabrice.drouin@acinq.fr>
2020-12-22 19:07:21 +0100

    Channel: fix publishing of final closing tx (fixes #143)

--- src/commonTest/kotlin/fr/acinq/eclair/channel/states/NegotiatingTestsCommon.kt
+++ src/commonTest/kotlin/fr/acinq/eclair/channel/states/NegotiatingTestsCommon.kt
@@ -1,16 +1,71 @@
 package fr.acinq.eclair.channel.states
 
-import fr.acinq.eclair.blockchain.BITCOIN_FUNDING_SPENT
-import fr.acinq.eclair.blockchain.WatchEventSpent
+import fr.acinq.bitcoin.Script
+import fr.acinq.bitcoin.ScriptFlags
+import fr.acinq.bitcoin.Transaction
+import fr.acinq.bitcoin.TxOut
+import fr.acinq.eclair.Eclair.randomKey
+import fr.acinq.eclair.blockchain.*
 import fr.acinq.eclair.channel.*
 import fr.acinq.eclair.channel.TestsHelper.mutualClose
 import fr.acinq.eclair.tests.utils.EclairTestSuite
+import fr.acinq.eclair.utils.toByteVector
 import fr.acinq.eclair.wire.ClosingSigned
+import fr.acinq.eclair.wire.Shutdown
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class NegotiatingTestsCommon : EclairTestSuite() {
+
+    @Test
+    fun `correctly sign and detect closing tx`() {
+        // we're fundee here, not funder !!
+        val (bob, alice) = TestsHelper.reachNormal()
+        val priv = randomKey()
+
+        // Alice initiates a mutual close with a custom final script
+        val finalScript = Script.write(Script.pay2pkh(priv.publicKey())).toByteVector()
+        val (alice1, actions1) = alice.process(ChannelEvent.ExecuteCommand(CMD_CLOSE(finalScript)))
+        val shutdownA = actions1.findOutgoingMessage<Shutdown>()
+
+        // Bob replies with Shutdown + ClosingSigned
+        val (bob1, actions2) = bob.process(ChannelEvent.MessageReceived(shutdownA))
+        val shutdownB = actions2.findOutgoingMessage<Shutdown>()
+        val closingSignedB = actions2.findOutgoingMessage<ClosingSigned>()
+
+        // Alice agrees with Bob's closing fee, publishes her closing tx and replies with her own ClosingSigned
+        val (alice2, _) = alice1.process(ChannelEvent.MessageReceived(shutdownB))
+        val (alice3, actions4) = alice2.process(ChannelEvent.MessageReceived(closingSignedB))
+        alice3 as Closing
+        val closingTxA = actions4.filterIsInstance<ChannelAction.Blockchain.PublishTx>().first().tx
+        val closingSignedA = actions4.findOutgoingMessage<ClosingSigned>()
+        val watch = actions4.findWatch<WatchConfirmed>()
+        assertEquals(watch.txId, closingTxA.txid)
+
+        val fundingTx = Transaction(
+            version = 2,
+            txIn = listOf(),
+            txOut = listOf(bob.commitments.commitInput.txOut),
+            lockTime = 0
+        )
+        (assertEquals(fundingTx.txid, closingTxA.txIn[0].outPoint.txid))
+        // check that our closing tx is correctly signed
+        Transaction.correctlySpends(closingTxA, fundingTx, ScriptFlags.STANDARD_SCRIPT_VERIFY_FLAGS)
+
+        // Bob published his closing tx (which should be the same as Alice's !!!)
+        val (bob2, actions5) = bob1.process(ChannelEvent.MessageReceived(closingSignedA))
+        assertTrue(bob2 is Closing)
+        val closingTxB = actions5.filterIsInstance<ChannelAction.Blockchain.PublishTx>().first().tx
+        assertEquals(closingTxA, closingTxB)
+
+        // Alice sees Bob's closing tx (which should be the same as the one she published)
+        val (alice4, _) = alice3.process(ChannelEvent.WatchReceived(WatchEventSpent(alice3.channelId, BITCOIN_FUNDING_SPENT, closingTxB)))
+        assertTrue(alice4 is Closing)
+
+        val (alice5, _) = alice4.process(ChannelEvent.WatchReceived(WatchEventConfirmed(alice3.channelId, BITCOIN_TX_CONFIRMED(closingTxA), 144, 0, closingTxA)))
+        assertTrue(alice5 is Closed)
+    }
 
     @Test
     fun `recv ClosingSigned (theirCloseFee != ourCloseFee)`() {
