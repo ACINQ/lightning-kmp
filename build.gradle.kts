be80f983303801268351624a08103bca30192e5e
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2021-01-27 17:50:12 +0100

    Upgrade kotlinx-coroutines-core to v1.4.2 (#183)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,14 @@ kotlin {
     fun ktor(module: String) = "io.ktor:ktor-$module:$ktorVersion"
     val secp256k1Version = "0.4.1"
     val serializationVersion = "1.0.0"
+    val coroutineVersion = "1.4.2-native-mt"
 
     val commonMain by sourceSets.getting {
         dependencies {
             api("fr.acinq.bitcoin:bitcoin-kmp:0.6.1")
             api("fr.acinq.secp256k1:secp256k1-kmp:$secp256k1Version")
             api("org.kodein.log:kodein-log:0.7.0")
-            api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9-native-mt-2")
+            api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
             api("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
             api("org.jetbrains.kotlinx:kotlinx-serialization-cbor:$serializationVersion")
             api("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
