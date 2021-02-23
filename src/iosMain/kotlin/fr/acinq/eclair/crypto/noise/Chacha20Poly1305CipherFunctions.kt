01a63c106cad13feecf526b57d8b96eb184cb3d0
Robbie Hanson <robbiehanson@deusty.com>
2021-02-23 09:53:20 -0500

    Removing on-the-fly unit testing

--- src/iosMain/kotlin/fr/acinq/eclair/crypto/noise/Chacha20Poly1305CipherFunctions.kt
+++ src/iosMain/kotlin/fr/acinq/eclair/crypto/noise/Chacha20Poly1305CipherFunctions.kt
@@ -35,67 +35,31 @@ fun ByteArray.toNSData(): NSData {
 actual object Chacha20Poly1305CipherFunctions : CipherFunctions {
     override fun name() = "ChaChaPoly"
 
-    val useNative = true
-    val testNative = false
-
     // as specified in BOLT #8
     fun nonce(n: Long): ByteArray = ByteArray(4) + ChaCha20Poly1305.write64(n)
 
     // Encrypts plaintext using the cipher key k of 32 bytes and an 8-byte unsigned integer nonce n which must be unique.
     override fun encrypt(k: ByteArray, n: Long, ad: ByteArray, plaintext: ByteArray): ByteArray {
 
-        var kResult: ByteArray? = null
-        if (!useNative || testNative) {
-            val (ciphertext, mac) = ChaCha20Poly1305.encrypt(
-                k, nonce(n), plaintext, ad
-            )
-            kResult = ciphertext + mac
-        }
-        if (useNative) {
-            val ciphertextAndMac = NativeChaChaPoly.chachapoly_encryptWithKey(
-                key = k.toNSData(),
-                nonce = nonce(n).toNSData(),
-                authenticatedData = ad.toNSData(),
-                plaintext = plaintext.toNSData()
-            )
-            val nResult = ciphertextAndMac.toByteArray()
-
-            if (testNative) {
-                require(kResult == nResult)
-            }
-            return nResult
-        } else {
-            return kResult!!
-        }
+        val ciphertextAndMac = NativeChaChaPoly.chachapoly_encryptWithKey(
+            key = k.toNSData(),
+            nonce = nonce(n).toNSData(),
+            authenticatedData = ad.toNSData(),
+            plaintext = plaintext.toNSData()
+        )
+        return ciphertextAndMac.toByteArray()
     }
 
     // Decrypts ciphertext using a cipher key k of 32 bytes, an 8-byte unsigned integer nonce n, and associated data ad.
     @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
     override fun decrypt(k: ByteArray, n: Long, ad: ByteArray, ciphertextAndMac: ByteArray): ByteArray {
 
-        var kResult: ByteArray? = null
-        if (!useNative || testNative) {
-            val ciphertext = ciphertextAndMac.dropLast(16).toByteArray()
-            val mac = ciphertextAndMac.takeLast(16).toByteArray()
-            kResult = ChaCha20Poly1305.decrypt(
-                k, nonce(n), ciphertext, ad, mac
-            )
-        }
-        if (useNative) {
-            val plaintext = NativeChaChaPoly.chachapoly_decryptWithKey(
-                key = k.toNSData(),
-                nonce = nonce(n).toNSData(),
-                authenticatedData = ad.toNSData(),
-                ciphertextAndTag = ciphertextAndMac.toNSData()
-            )
-            val nResult = plaintext.toByteArray()
-
-            if (testNative) {
-                require(kResult == nResult)
-            }
-            return nResult
-        } else {
-            return kResult!!
-        }
+        val plaintext = NativeChaChaPoly.chachapoly_decryptWithKey(
+            key = k.toNSData(),
+            nonce = nonce(n).toNSData(),
+            authenticatedData = ad.toNSData(),
+            ciphertextAndTag = ciphertextAndMac.toNSData()
+        )
+        return plaintext.toByteArray()
     }
 }
