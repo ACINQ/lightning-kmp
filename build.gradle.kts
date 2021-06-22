3ee750bdf16e6fb1c96f7f1116b1efae0808fc45
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2021-06-22 14:21:17 +0200

    Use forked version of kodein-log with UTC timezone only (#273)
    Fixes https://github.com/ACINQ/phoenix-kmm/issues/148

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ kotlin {
         dependencies {
             api("fr.acinq.bitcoin:bitcoin-kmp:0.7.0")
             api("fr.acinq.secp256k1:secp256k1-kmp:$secp256k1Version")
-            api("org.kodein.log:kodein-log:0.10.1")
+            api("org.kodein.log:kodein-log:0.10.1-utc")
             api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
             api("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
             api("org.jetbrains.kotlinx:kotlinx-serialization-cbor:$serializationVersion")
