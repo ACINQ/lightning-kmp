ACINQ/lightning-kmp

commit f699f64837a011b944a373e1821b2694e5dbe88f

Fix pay-to-open below minimum when using MPP (#283)
When using a mix of normal HTLC and pay-to-open in an MPP payment,
we incorrectly compared the minimum funding amount to the payment total amount.

We must instead compare it to the amount offered via pay-to-open (excluding
HTLCs from this check).

We also shouldn't accept a payment from a pay-to-open below our min-funding-amount,
because we will reject the corresponding `open_channel` message.

src/commonMain/kotlin/fr/acinq/lightning/payment/IncomingPaymentHandler.kt
@@ -202,6 +202,7 @@ class IncomingPaymentHandler(val nodeParams: NodeParams, val walletParams: Walle
                 } else {
                     val payment = pending[paymentPart.paymentHash]?.add(paymentPart) ?: PendingPayment(paymentPart)
                     val payToOpenMinAmount = payment.parts.filterIsInstance<PayToOpenPart>().map { it.payToOpenRequest.payToOpenMinAmountMsat }.firstOrNull()
+                    val payToOpenAmount = payment.parts.filterIsInstance<PayToOpenPart>().map { it.payToOpenRequest.amountMsat }.sum()
                     when {
                         paymentPart.totalAmount != payment.totalAmount -> {
                             // Bolt 04:
@@ -222,7 +223,7 @@ class IncomingPaymentHandler(val nodeParams: NodeParams, val walletParams: Walle
                             pending[paymentPart.paymentHash] = payment
                             return ProcessAddResult.Pending(incomingPayment)
                         }
-                        payToOpenMinAmount is MilliSatoshi && payment.amountReceived < payToOpenMinAmount -> {
+                        payToOpenMinAmount != null && (payToOpenAmount < payToOpenMinAmount || payToOpenAmount < nodeParams.minFundingSatoshis) -> {
                             // Because of the cost of opening a new channel, there is a minimum amount for incoming payments to trigger
                             // a pay-to-open. Given that the total amount of a payment is included in each payment part, we could have
                             // rejected pay-to-open parts as they arrived, but it would have caused two issues:
@@ -232,7 +233,7 @@ class IncomingPaymentHandler(val nodeParams: NodeParams, val walletParams: Walle
                             //   regarding the failed pay-to-open
                             // That is why, instead, we wait for all parts to arrive. Then, if there is at least one pay-to-open part, and if
                             // the total received amount is less than the minimum amount required for a pay-to-open, we fail the payment.
-                            logger.warning { "h:${paymentPart.paymentHash} amount received is too low for a pay-to-open (minimum ${payToOpenMinAmount}, received ${payment.amountReceived})" }
+                            logger.warning { "h:${paymentPart.paymentHash} amount received is too low for a pay-to-open (minimum ${payToOpenMinAmount}, minimum funding amount ${nodeParams.minFundingSatoshis}, received ${payToOpenAmount})" }
                             val actions = payment.parts.map { part ->
                                 val failureMsg = IncorrectOrUnknownPaymentDetails(part.totalAmount, currentBlockHeight.toLong())
                                 when (part) {

src/commonMain/kotlin/fr/acinq/lightning/wire/LightningMessages.kt
@@ -1038,7 +1038,7 @@ data class ClosingSigned(
  * @param chainHash chain we're on.
  * @param fundingSatoshis total capacity of the channel our peer will open to us (some of the funds may be on their side).
  * @param amountMsat payment amount covered by this new channel: we will receive push_msat = amountMsat - fees.
- * @param payToOpenMinAmountMsat minimum amount for a pay-to-open to be attempted, this should be compared to the total amount in the case of an AMP payment.
+ * @param payToOpenMinAmountMsat minimum amount for a pay-to-open to be attempted, this should be compared to the total amount in the case of an MPP payment.
  * @param payToOpenFeeSatoshis fees that will be deducted from the amount pushed to us (this fee covers the on-chain fees our peer will pay to open the channel).
  * @param paymentHash payment hash.
  * @param expireAt after the proposal expires, our peer will fail the payment and won't open a channel to us.

commit b69fd6fc9dee30227a7da1bc20e087c66330a6a5

do not compare pay-to-open amount to min-funding
This is a regression introduced by #283. We should use
`PayToOpenRequest.fundingSatoshis`, not `PayToOpenRequest.amountMsat`
when comparing to `nodeParams.minFundingSatoshis`.

NB: this operates on a per-payment-part basis, and in the case of an AMP
payment, the final funding amount will be different, but it won't be
smaller than the one of each single part. If it were, because of a bug in
the peer, the channel would be rejected by Phoenix at opening and that's
okay. The peer would retry until a correct parameter is set and Phoenix
accepts the channel.

In theory we could rely on the `PayToOpenRequest.combine` method to
compute the final `fundingSatoshi`, but there is no guarantee that
Phoenix has the same implementation than the peer so it's not better.
The `combine()` wasn't used and has been removed.

src/commonMain/kotlin/fr/acinq/lightning/payment/IncomingPaymentHandler.kt
@@ -223,7 +223,7 @@ class IncomingPaymentHandler(val nodeParams: NodeParams, val walletParams: Walle
                             pending[paymentPart.paymentHash] = payment
                             return ProcessAddResult.Pending(incomingPayment)
                         }
-                        payToOpenMinAmount != null && (payToOpenAmount < payToOpenMinAmount || payToOpenAmount < nodeParams.minFundingSatoshis) -> {
+                        payToOpenMinAmount != null && payToOpenAmount < payToOpenMinAmount -> {
                             // Because of the cost of opening a new channel, there is a minimum amount for incoming payments to trigger
                             // a pay-to-open. Given that the total amount of a payment is included in each payment part, we could have
                             // rejected pay-to-open parts as they arrived, but it would have caused two issues:
@@ -233,7 +233,7 @@ class IncomingPaymentHandler(val nodeParams: NodeParams, val walletParams: Walle
                             //   regarding the failed pay-to-open
                             // That is why, instead, we wait for all parts to arrive. Then, if there is at least one pay-to-open part, and if
                             // the total received amount is less than the minimum amount required for a pay-to-open, we fail the payment.
-                            logger.warning { "h:${paymentPart.paymentHash} amount received is too low for a pay-to-open (minimum ${payToOpenMinAmount}, minimum funding amount ${nodeParams.minFundingSatoshis}, received ${payToOpenAmount})" }
+                            logger.warning { "h:${paymentPart.paymentHash} amount received is too low for a pay-to-open (minimum ${payToOpenMinAmount}, received ${payToOpenAmount})" }
                             val actions = payment.parts.map { part ->
                                 val failureMsg = IncorrectOrUnknownPaymentDetails(part.totalAmount, currentBlockHeight.toLong())
                                 when (part) {
@@ -333,11 +333,15 @@ class IncomingPaymentHandler(val nodeParams: NodeParams, val walletParams: Walle
                 //
                 //   Note: this allows the origin node to reduce information leakage by altering
                 //   the amount while not allowing for accidental gross overpayment.
-                logger.warning { "h:${paymentPart.paymentHash} received invalid amount (overpayment): ${paymentPart.totalAmount}, expected: ${incomingPayment.origin.paymentRequest.amount}" }
+                logger.warning { "h:${paymentPart.paymentHash} invalid amount (overpayment): ${paymentPart.totalAmount}, expected: ${incomingPayment.origin.paymentRequest.amount}" }
                 Either.Left(rejectPaymentPart(privateKey, paymentPart, incomingPayment, currentBlockHeight))
             }
             paymentPart is HtlcPart && paymentPart.htlc.cltvExpiry < minFinalCltvExpiry(incomingPayment.origin.paymentRequest, currentBlockHeight) -> {
-                logger.warning { "h:${paymentPart.paymentHash} received payment with expiry too small: received ${paymentPart.htlc.cltvExpiry}, min is ${minFinalCltvExpiry(incomingPayment.origin.paymentRequest, currentBlockHeight)}" }
+                logger.warning { "h:${paymentPart.paymentHash} payment with expiry too small: received ${paymentPart.htlc.cltvExpiry}, min is ${minFinalCltvExpiry(incomingPayment.origin.paymentRequest, currentBlockHeight)}" }
+                Either.Left(rejectPaymentPart(privateKey, paymentPart, incomingPayment, currentBlockHeight))
+            }
+            paymentPart is PayToOpenPart && paymentPart.payToOpenRequest.fundingSatoshis < nodeParams.minFundingSatoshis -> {
+                logger.warning { "h:${paymentPart.payToOpenRequest.paymentHash} received invalid funding amount for a pay-to-open: ${paymentPart.payToOpenRequest.fundingSatoshis}, min is ${nodeParams.minFundingSatoshis}" }
                 Either.Left(rejectPaymentPart(privateKey, paymentPart, incomingPayment, currentBlockHeight))
             }
             else -> Either.Right(incomingPayment)

src/commonMain/kotlin/fr/acinq/lightning/wire/LightningMessages.kt
@@ -1084,23 +1084,6 @@ data class PayToOpenRequest(
                 finalPacket = OnionRoutingPacketSerializer(LightningCodecs.u16(input)).read(input)
             )
         }
-
-        /**
-         * We use this method for non-trampoline multipart payments.
-         * The aggregation is done by the wallet, which may receive several pay-to-open requests for the same payment.
-         * Combining the requests allows us to create a single channel.
-         *
-         * We return the total amount for this set of pay-to-open requests, and the fee that will be deducted when the channel will be opened.
-         */
-        fun combine(requests: List<PayToOpenRequest>): Pair<MilliSatoshi, Satoshi> {
-            require(requests.isNotEmpty()) { "there needs to be at least one pay-to-open request" }
-            require(requests.map { it.chainHash }.toSet().size == 1) { "all pay-to-open chain hash must be equal" }
-            require(requests.map { it.paymentHash }.toSet().size == 1) { "all pay-to-open payment hash must be equal" }
-            require(requests.map { it.payToOpenMinAmountMsat }.toSet().size == 1) { "all pay-to-open min amounts must be equal" }
-            val totalAmount = requests.map { it.amountMsat }.sum()
-            val fees = requests.map { it.payToOpenFeeSatoshis }.sum()
-            return Pair(totalAmount, fees)
-        }
     }
 }
 

src/commonTest/kotlin/fr/acinq/lightning/payment/IncomingPaymentHandlerTestsCommon.kt
@@ -259,6 +259,39 @@ class IncomingPaymentHandlerTestsCommon : LightningTestSuite() {
     }
 
     @Test
+    fun `receive pay-to-open payment with a funding amount too low`() = runSuspendTest {
+        val (paymentHandler, incomingPayment, paymentSecret) = run {
+            val (paymentHandler, incomingPayment, paymentSecret) = createFixture(defaultAmount)
+            val paymentHandler1 = IncomingPaymentHandler(paymentHandler.nodeParams.copy(minFundingSatoshis = 200_000.sat), paymentHandler.walletParams, paymentHandler.db)
+            Triple(paymentHandler1, incomingPayment, paymentSecret)
+        }
+
+        val payToOpenRequest = makePayToOpenRequest(incomingPayment, makeMppPayload(defaultAmount, defaultAmount, paymentSecret))
+        // we make sure that the pay-to-open funding amount is invalid
+        assertTrue { payToOpenRequest.fundingSatoshis < paymentHandler.nodeParams.minFundingSatoshis }
+        val result = paymentHandler.process(payToOpenRequest, TestConstants.defaultBlockHeight)
+
+        assertTrue { result is IncomingPaymentHandler.ProcessAddResult.Rejected }
+        result as IncomingPaymentHandler.ProcessAddResult.Rejected
+        assertEquals(incomingPayment, result.incomingPayment)
+        val expected = PayToOpenResponseEvent(
+            PayToOpenResponse(
+                payToOpenRequest.chainHash,
+                payToOpenRequest.paymentHash,
+                PayToOpenResponse.Result.Failure(
+                    OutgoingPacket.buildHtlcFailure(
+                        paymentHandler.nodeParams.nodePrivateKey,
+                        payToOpenRequest.paymentHash,
+                        payToOpenRequest.finalPacket,
+                        CMD_FAIL_HTLC.Reason.Failure(IncorrectOrUnknownPaymentDetails(payToOpenRequest.amountMsat, TestConstants.defaultBlockHeight.toLong()))
+                    ).right!!
+                )
+            )
+        )
+        assertEquals(setOf(expected), result.actions.toSet())
+    }
+
+    @Test
     fun `receive pay-to-open trampoline payment with an incorrect payment secret`() = runSuspendTest {
         val (paymentHandler, incomingPayment, paymentSecret) = createFixture(defaultAmount)
         val trampolineHops = listOf(
@@ -587,83 +620,6 @@ class IncomingPaymentHandlerTestsCommon : LightningTestSuite() {
         // - Bob has received the complete MPP set
         run {
             val payToOpenRequest2 = makePayToOpenRequest(incomingPayment, makeMppPayload(amount3, totalAmount, paymentSecret), payToOpenMinAmount = 75_000.msat)
-            val result = paymentHandler.process(payToOpenRequest2, TestConstants.defaultBlockHeight)
-            assertTrue { result is IncomingPaymentHandler.ProcessAddResult.Rejected }
-            val expected = setOf(
-                WrappedChannelEvent(
-                    channelId,
-                    ChannelEvent.ExecuteCommand(CMD_FAIL_HTLC(0, CMD_FAIL_HTLC.Reason.Failure(IncorrectOrUnknownPaymentDetails(totalAmount, TestConstants.defaultBlockHeight.toLong())), commit = true))
-                ),
-                PayToOpenResponseEvent(
-                    PayToOpenResponse(
-                        payToOpenRequest1.chainHash,
-                        payToOpenRequest1.paymentHash,
-                        PayToOpenResponse.Result.Failure(
-                            OutgoingPacket.buildHtlcFailure(
-                                paymentHandler.nodeParams.nodePrivateKey,
-                                payToOpenRequest1.paymentHash,
-                                payToOpenRequest1.finalPacket,
-                                CMD_FAIL_HTLC.Reason.Failure(IncorrectOrUnknownPaymentDetails(totalAmount, TestConstants.defaultBlockHeight.toLong()))
-                            ).right!!
-                        )
-                    )
-                ),
-                PayToOpenResponseEvent(
-                    PayToOpenResponse(
-                        payToOpenRequest2.chainHash,
-                        payToOpenRequest2.paymentHash,
-                        PayToOpenResponse.Result.Failure(
-                            OutgoingPacket.buildHtlcFailure(
-                                paymentHandler.nodeParams.nodePrivateKey,
-                                payToOpenRequest2.paymentHash,
-                                payToOpenRequest2.finalPacket,
-                                CMD_FAIL_HTLC.Reason.Failure(IncorrectOrUnknownPaymentDetails(totalAmount, TestConstants.defaultBlockHeight.toLong()))
-                            ).right!!
-                        )
-                    )
-                ),
-            )
-            assertEquals(expected, result.actions.toSet())
-        }
-    }
-
-    @Test
-    fun `receive multipart payment with a mix of HTLC and pay-to-open (amount of the pay-to-open parts lower than minimum funding)`() = runSuspendTest {
-        val channelId = randomBytes32()
-        val (amount1, amount2, amount3) = Triple(100_000.msat, 30_000.msat, 20_000.msat)
-        val totalAmount = amount1 + amount2 + amount3
-        val (paymentHandler, incomingPayment, paymentSecret) = run {
-            val (paymentHandler, incomingPayment, paymentSecret) = createFixture(totalAmount)
-            val paymentHandler1 = IncomingPaymentHandler(paymentHandler.nodeParams.copy(minFundingSatoshis = 75.sat), paymentHandler.walletParams, paymentHandler.db)
-            Triple(paymentHandler1, incomingPayment, paymentSecret)
-        }
-
-        // Step 1 of 3:
-        // - Alice sends first multipart htlc to Bob
-        // - Bob doesn't accept the MPP set yet
-        run {
-            val add = makeUpdateAddHtlc(0, channelId, paymentHandler, incomingPayment.paymentHash, makeMppPayload(amount1, totalAmount, paymentSecret))
-            val result = paymentHandler.process(add, TestConstants.defaultBlockHeight)
-            assertTrue { result is IncomingPaymentHandler.ProcessAddResult.Pending }
-            assertTrue { result.actions.isEmpty() }
-        }
-
-        // Step 2 of 3:
-        // - Alice sends second multipart htlc to Bob
-        // - Bob doesn't accept the MPP set yet
-        val payToOpenRequest1 = run {
-            val payToOpenRequest = makePayToOpenRequest(incomingPayment, makeMppPayload(amount2, totalAmount, paymentSecret), payToOpenMinAmount = 25_000.msat)
-            val result = paymentHandler.process(payToOpenRequest, TestConstants.defaultBlockHeight)
-            assertTrue { result is IncomingPaymentHandler.ProcessAddResult.Pending }
-            assertTrue { result.actions.isEmpty() }
-            payToOpenRequest
-        }
-
-        // Step 3 of 3:
-        // - Alice sends last multipart htlc to Bob
-        // - Bob has received the complete MPP set
-        run {
-            val payToOpenRequest2 = makePayToOpenRequest(incomingPayment, makeMppPayload(amount3, totalAmount, paymentSecret), payToOpenMinAmount = 25_000.msat)
             val result = paymentHandler.process(payToOpenRequest2, TestConstants.defaultBlockHeight)
             assertTrue { result is IncomingPaymentHandler.ProcessAddResult.Rejected }
             val expected = setOf(