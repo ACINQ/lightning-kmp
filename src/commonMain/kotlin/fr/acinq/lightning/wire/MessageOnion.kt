3a0d411d3d83187ece870803c0424834f9ed3eb3
Thomas HUET <81159533+thomash-acinq@users.noreply.github.com>
2022-06-28 07:21:14 +0200

    Add codecs for onion messages (#336)
    Add codecs for onion messages
    
    but do not decrypt them yet

---
+++ src/commonMain/kotlin/fr/acinq/lightning/wire/MessageOnion.kt
@@ -0,0 +1,92 @@
+package fr.acinq.lightning.wire
+
+import fr.acinq.bitcoin.ByteVector
+import fr.acinq.bitcoin.PublicKey
+import fr.acinq.bitcoin.io.ByteArrayInput
+import fr.acinq.bitcoin.io.ByteArrayOutput
+import fr.acinq.bitcoin.io.Input
+import fr.acinq.bitcoin.io.Output
+import fr.acinq.lightning.crypto.RouteBlinding
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+
+@OptIn(ExperimentalUnsignedTypes::class)
+@Serializable
+sealed class OnionMessagePayloadTlv : Tlv {
+    /**
+     * Onion messages may provide a reply path, allowing the recipient to send a message back to the original sender.
+     * The reply path uses route blinding, which ensures that the sender doesn't leak its identity to the recipient.
+     */
+    @Serializable
+    data class ReplyPath(@Contextual val blindedRoute: RouteBlinding.BlindedRoute) : OnionMessagePayloadTlv() {
+        override val tag: Long get() = ReplyPath.tag
+        override fun write(out: Output) {
+            LightningCodecs.writeBytes(blindedRoute.introductionNodeId.value, out)
+            LightningCodecs.writeBytes(blindedRoute.blindingKey.value, out)
+            for (hop in blindedRoute.blindedNodes) {
+                LightningCodecs.writeBytes(hop.blindedPublicKey.value, out)
+                LightningCodecs.writeU16(hop.encryptedPayload.size(), out)
+                LightningCodecs.writeBytes(hop.encryptedPayload, out)
+            }
+        }
+
+        companion object : TlvValueReader<ReplyPath> {
+            const val tag: Long = 2
+            override fun read(input: Input): ReplyPath {
+                val firstNodeId = PublicKey(LightningCodecs.bytes(input, 33))
+                val blinding = PublicKey(LightningCodecs.bytes(input, 33))
+                val path = sequence {
+                    while (input.availableBytes > 0) {
+                        val blindedPublicKey = PublicKey(LightningCodecs.bytes(input, 33))
+                        val encryptedPayload = ByteVector(LightningCodecs.bytes(input, LightningCodecs.u16(input)))
+                        yield(RouteBlinding.BlindedNode(blindedPublicKey, encryptedPayload))
+                    }
+                }.toList()
+                return ReplyPath(RouteBlinding.BlindedRoute(firstNodeId, blinding, path))
+            }
+        }
+    }
+
+    /**
+     * Onion messages always use route blinding, even in the forward direction.
+     * This ensures that intermediate nodes can't know whether they're forwarding a message or its reply.
+     * The sender must provide some encrypted data for each intermediate node which lets them locate the next node.
+     */
+    @Serializable
+    data class EncryptedData(@Contextual val data: ByteVector) : OnionMessagePayloadTlv() {
+        override val tag: Long get() = EncryptedData.tag
+        override fun write(out: Output) = LightningCodecs.writeBytes(data, out)
+
+        companion object : TlvValueReader<EncryptedData> {
+            const val tag: Long = 4
+            override fun read(input: Input): EncryptedData =
+                EncryptedData(ByteVector(LightningCodecs.bytes(input, input.availableBytes)))
+        }
+    }
+}
+
+data class MessageOnion(val records: TlvStream<OnionMessagePayloadTlv>) {
+    val replyPath = records.get<OnionMessagePayloadTlv.ReplyPath>()?.blindedRoute
+    val encryptedData = records.get<OnionMessagePayloadTlv.EncryptedData>()!!.data
+
+    fun write(out: Output) = tlvSerializer.write(records, out)
+
+    fun write(): ByteArray {
+        val out = ByteArrayOutput()
+        write(out)
+        return out.toByteArray()
+    }
+
+    companion object {
+        val tlvSerializer = TlvStreamSerializer(
+            true, @Suppress("UNCHECKED_CAST") mapOf(
+                OnionMessagePayloadTlv.ReplyPath.tag to OnionMessagePayloadTlv.ReplyPath.Companion as TlvValueReader<OnionMessagePayloadTlv>,
+                OnionMessagePayloadTlv.EncryptedData.tag to OnionMessagePayloadTlv.EncryptedData.Companion as TlvValueReader<OnionMessagePayloadTlv>,
+            )
+        )
+
+        fun read(input: Input): MessageOnion = MessageOnion(tlvSerializer.read(input))
+
+        fun read(bytes: ByteArray): MessageOnion = read(ByteArrayInput(bytes))
+    }
+}
---
+++ src/commonMain/kotlin/fr/acinq/lightning/wire/RouteBlinding.kt
@@ -0,0 +1,99 @@
+package fr.acinq.lightning.wire
+
+import fr.acinq.bitcoin.ByteVector
+import fr.acinq.bitcoin.PublicKey
+import fr.acinq.bitcoin.io.ByteArrayInput
+import fr.acinq.bitcoin.io.ByteArrayOutput
+import fr.acinq.bitcoin.io.Input
+import fr.acinq.bitcoin.io.Output
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class RouteBlindingEncryptedDataTlv : Tlv {
+    /** Some padding can be added to ensure all payloads are the same size to improve privacy. */
+    @Serializable
+    data class Padding(@Contextual val dummy: ByteVector) : RouteBlindingEncryptedDataTlv() {
+        override val tag: Long get() = Padding.tag
+        override fun write(out: Output) = LightningCodecs.writeBytes(dummy, out)
+
+        companion object : TlvValueReader<Padding> {
+            const val tag: Long = 1
+            override fun read(input: Input): Padding =
+                Padding(ByteVector(LightningCodecs.bytes(input, input.availableBytes)))
+        }
+    }
+
+    /** Id of the next node. */
+    @Serializable
+    data class OutgoingNodeId(@Contextual val nodeId: PublicKey) : RouteBlindingEncryptedDataTlv() {
+        override val tag: Long get() = OutgoingNodeId.tag
+        override fun write(out: Output) = LightningCodecs.writeBytes(nodeId.value, out)
+
+        companion object : TlvValueReader<OutgoingNodeId> {
+            const val tag: Long = 4
+            override fun read(input: Input): OutgoingNodeId =
+                OutgoingNodeId(PublicKey(LightningCodecs.bytes(input, 33)))
+        }
+    }
+
+    /**
+     * The final recipient may store some data in the encrypted payload for itself to avoid storing it locally.
+     * It can for example put a payment_hash to verify that the route is used for the correct invoice.
+     * It should use that field to detect when blinded routes are used outside of their intended use (malicious probing)
+     * and react accordingly (ignore the message or send an error depending on the use-case).
+     */
+    @Serializable
+    data class PathId(@Contextual val data: ByteVector) : RouteBlindingEncryptedDataTlv() {
+        override val tag: Long get() = PathId.tag
+        override fun write(out: Output) = LightningCodecs.writeBytes(data, out)
+
+        companion object : TlvValueReader<PathId> {
+            const val tag: Long = 6
+            override fun read(input: Input): PathId =
+                PathId(ByteVector(LightningCodecs.bytes(input, input.availableBytes)))
+        }
+    }
+
+    /** Blinding override for the rest of the route. */
+    @Serializable
+    data class NextBlinding(@Contextual val blinding: PublicKey) : RouteBlindingEncryptedDataTlv() {
+        override val tag: Long get() = NextBlinding.tag
+        override fun write(out: Output) = LightningCodecs.writeBytes(blinding.value, out)
+
+        companion object : TlvValueReader<NextBlinding> {
+            const val tag: Long = 8
+            override fun read(input: Input): NextBlinding =
+                NextBlinding(PublicKey(LightningCodecs.bytes(input, 33)))
+        }
+    }
+}
+
+data class RouteBlindingEncryptedData(val records: TlvStream<RouteBlindingEncryptedDataTlv>) {
+    val nextNodeId = records.get<RouteBlindingEncryptedDataTlv.OutgoingNodeId>()?.nodeId
+    val pathId = records.get<RouteBlindingEncryptedDataTlv.PathId>()?.data
+    val nextBlindingOverride = records.get<RouteBlindingEncryptedDataTlv.NextBlinding>()?.blinding
+
+    fun write(out: Output) = tlvSerializer.write(records, out)
+
+    fun write(): ByteArray {
+        val out = ByteArrayOutput()
+        write(out)
+        return out.toByteArray()
+    }
+
+    companion object {
+        val tlvSerializer = TlvStreamSerializer(
+            false, @Suppress("UNCHECKED_CAST") mapOf(
+                RouteBlindingEncryptedDataTlv.Padding.tag to RouteBlindingEncryptedDataTlv.Padding.Companion as TlvValueReader<RouteBlindingEncryptedDataTlv>,
+                RouteBlindingEncryptedDataTlv.OutgoingNodeId.tag to RouteBlindingEncryptedDataTlv.OutgoingNodeId.Companion as TlvValueReader<RouteBlindingEncryptedDataTlv>,
+                RouteBlindingEncryptedDataTlv.PathId.tag to RouteBlindingEncryptedDataTlv.PathId.Companion as TlvValueReader<RouteBlindingEncryptedDataTlv>,
+                RouteBlindingEncryptedDataTlv.NextBlinding.tag to RouteBlindingEncryptedDataTlv.NextBlinding.Companion as TlvValueReader<RouteBlindingEncryptedDataTlv>,
+            )
+        )
+
+        fun read(input: Input): RouteBlindingEncryptedData = RouteBlindingEncryptedData(tlvSerializer.read(input))
+
+        fun read(bytes: ByteArray): RouteBlindingEncryptedData = read(ByteArrayInput(bytes))
+    }
+}
