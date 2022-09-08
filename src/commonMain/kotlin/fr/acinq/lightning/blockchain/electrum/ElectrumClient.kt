862b9f0575347902ef54cde67b28d1544d3ff8c4
Dominique <5765435+dpad85@users.noreply.github.com>
2022-09-08 11:34:17 +0200

    Update connection state when electrum disconnects

--- src/commonMain/kotlin/fr/acinq/lightning/blockchain/electrum/ElectrumClient.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/blockchain/electrum/ElectrumClient.kt
@@ -251,6 +251,7 @@ class ElectrumClient(
 
     fun disconnect() {
         if (this::socket.isInitialized) socket.close()
+        _connectionState.value = Connection.CLOSED(null)
         output.close()
     }
 
