fbb1eed182894f73d70565bd7814fccf1d0987f8
pm47 <pm.padiou@gmail.com>
2022-09-23 10:47:40 +0200

    rework test

--- src/commonTest/kotlin/fr/acinq/lightning/blockchain/electrum/ElectrumMiniWalletIntegrationTest.kt
+++ src/commonTest/kotlin/fr/acinq/lightning/blockchain/electrum/ElectrumMiniWalletIntegrationTest.kt
@@ -1,6 +1,8 @@
 package fr.acinq.lightning.blockchain.electrum
 
+import fr.acinq.bitcoin.Bitcoin
 import fr.acinq.bitcoin.Block
+import fr.acinq.bitcoin.ByteVector32
 import fr.acinq.bitcoin.Transaction
 import fr.acinq.lightning.Lightning.randomKey
 import fr.acinq.lightning.io.TcpSocket
@@ -73,15 +75,18 @@ class ElectrumMiniWalletIntegrationTest : LightningTestSuite() {
         val wallet = ElectrumMiniWallet(Block.LivenetGenesisBlock.hash, client, this)
         wallet.addAddress("16MmJT8VqW465GEyckWae547jKVfMB14P8")
         wallet.addAddress("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", randomKey())
-        wallet.addAddress("19b3QuFuYSSYPoLt1AxVQmG3LifgSSPNyA", randomKey())
+        wallet.addAddress("1NHFyu1uJ1UoDjtPjqZ4Et3wNCyMGCJ1qV", randomKey())
 
         val walletState = wallet.walletStateFlow
             .filter { it.parentTxs.size == 11 }
             .first()
 
+        // this has been checked on the blockchain
         assertEquals(4 + 6 + 1, walletState.utxos.size)
-        assertEquals(72_000_000.sat + 30_000_000.sat + 5_000_000.sat, walletState.balance)
+        assertEquals(72_000_000.sat + 30_000_000.sat + 2_000_000.sat, walletState.balance)
         assertEquals(7, walletState.spendable().size)
+        // make sure txid is correct has electrum api is confusing
+        walletState.parentTxs.forEach { assertEquals(it.key, it.value.txid) }
         assertContains(
             walletState.spendable(),
             WalletState.Utxo(
@@ -90,6 +95,24 @@ class ElectrumMiniWalletIntegrationTest : LightningTestSuite() {
                 blockHeight = 100_003
             )
         )
+
+        assertEquals(
+            expected = setOf(
+                Triple("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", ByteVector32.fromValidHex("71b3dbaca67e9f9189dad3617138c19725ab541ef0b49c05a94913e9f28e3f4e") to 0, 5_000_000.sat),
+                Triple("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", ByteVector32.fromValidHex("21d2eb195736af2a40d42107e6abd59c97eb6cffd4a5a7a7709e86590ae61987") to 0, 5_000_000.sat),
+                Triple("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", ByteVector32.fromValidHex("74d681e0e03bafa802c8aa084379aa98d9fcd632ddc2ed9782b586ec87451f20") to 1, 5_000_000.sat),
+                Triple("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", ByteVector32.fromValidHex("563ea83f9641d37a36f9294d172fdb4fb86c19b0e9cac45e0b27610331138775") to 0, 5_000_000.sat),
+                Triple("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", ByteVector32.fromValidHex("971af80218684017722429be08548d1f30a2f1f220abc064380cbca5cabf7623") to 1, 5_000_000.sat),
+                Triple("14xb2HATmkBzrHf4CR2hZczEtjYpTh92d2", ByteVector32.fromValidHex("b1ec9c44009147f3cee26caba45abec2610c74df9751fad14074119b5314da21") to 0, 5_000_000.sat),
+                Triple("1NHFyu1uJ1UoDjtPjqZ4Et3wNCyMGCJ1qV", ByteVector32.fromValidHex("602839d82ac6c9aafd1a20fff5b23e11a99271e7cc238d2e48b352219b2b87ab") to 1, 2_000_000.sat),
+            ),
+            actual = walletState.spendable().map {
+                val txOut = it.previousTx.txOut[it.outputIndex]
+                val address = Bitcoin.addressFromPublicKeyScript(Block.LivenetGenesisBlock.hash, txOut.publicKeyScript.toByteArray())
+                Triple(address, it.previousTx.txid to it.outputIndex, txOut.amount)
+            }.toSet()
+        )
+
         client.stop()
     }
 }
