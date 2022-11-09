1d39974c91b897b40c84fa1fb4ee50e63b95d995
pm47 <pm.padiou@gmail.com>
2022-11-09 11:10:41 +0100

    apply @sstone suggestion for reading version

--- src/commonMain/kotlin/fr/acinq/lightning/serialization/Serialization.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/serialization/Serialization.kt
@@ -1,5 +1,6 @@
 package fr.acinq.lightning.serialization
 
+import fr.acinq.bitcoin.crypto.Pack
 import fr.acinq.lightning.channel.PersistedChannelState
 import fr.acinq.lightning.serialization.v4.Deserialization.fromBinV4
 import fr.acinq.lightning.serialization.v4.Serialization.toBinV4
@@ -15,8 +16,8 @@ object Serialization {
             // v4 uses a 1-byte version discriminator
             bin[0].toInt() == 4 -> bin.fromBinV4()
             // v2/v3 use a 4-bytes version discriminator
-            bin.sliceArray(0..3).contentEquals(byteArrayOf(0, 0, 0, 3)) -> fr.acinq.lightning.serialization.v3.Serialization.deserialize(bin)
-            bin.sliceArray(0..3).contentEquals(byteArrayOf(0, 0, 0, 2)) -> fr.acinq.lightning.serialization.v2.Serialization.deserialize(bin)
+            Pack.int32BE(bin) == 3 -> fr.acinq.lightning.serialization.v3.Serialization.deserialize(bin)
+            Pack.int32BE(bin) == 2  -> fr.acinq.lightning.serialization.v2.Serialization.deserialize(bin)
             else -> error("unknown serialization version")
         }
     }
