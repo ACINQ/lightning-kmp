28840dc616b8694a04472e4a54e80f8386c35bbc
Dominique Padiou <5765435+dpad85@users.noreply.github.com>
2023-03-08 14:04:55 +0100

    Publish `NodeEvent` when a pay-to-open is rejected (#424)
    Only the pay-to-open rejection scenario implemented, and only when 
    it's because the amount is below minimum. This event is emitted by
    the incoming payment handler.

--- src/commonMain/kotlin/fr/acinq/lightning/NodeEvents.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/NodeEvents.kt
@@ -5,6 +5,7 @@ import fr.acinq.bitcoin.Satoshi
 import fr.acinq.lightning.channel.ChannelStateWithCommitments
 import fr.acinq.lightning.channel.Normal
 import fr.acinq.lightning.channel.WaitForFundingCreated
+import fr.acinq.lightning.wire.PayToOpenRequest
 import fr.acinq.lightning.wire.PleaseOpenChannel
 import fr.acinq.lightning.wire.PleaseOpenChannelFailure
 
@@ -20,4 +21,15 @@ sealed interface ChannelEvents : NodeEvents {
     data class Creating(val state: WaitForFundingCreated) : ChannelEvents
     data class Created(val state: ChannelStateWithCommitments) : ChannelEvents
     data class Confirmed(val state: Normal) : ChannelEvents
+}
+
+sealed interface PayToOpenEvents : NodeEvents {
+    val paymentHash: ByteVector32
+    /** Total incoming amount, incoming htlcs parts. */
+    val totalAmount: MilliSatoshi
+    /** Total amount for pay-to-open parts. */
+    val payToOpenAmount: MilliSatoshi
+    sealed class Rejected: PayToOpenEvents {
+        data class BelowMin(override val paymentHash: ByteVector32, override val totalAmount: MilliSatoshi, override val payToOpenAmount: MilliSatoshi, val payToOpenMinAmount: MilliSatoshi): Rejected()
+    }
 }
