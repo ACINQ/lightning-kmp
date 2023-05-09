79f28e3f36b3376306bf7767764b7d31567522b4
sstone <fabrice.drouin@acinq.fr>
2023-05-09 20:24:59 +0200

    Update bitcoin-kmp and secp256k1-kmp

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
 
 plugins {
-    kotlin("multiplatform") version "1.8.10"
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("multiplatform") version "1.8.21"
+    kotlin("plugin.serialization") version "1.8.21"
     id("org.jetbrains.dokka") version "1.8.10"
     `maven-publish`
 }
@@ -14,7 +14,7 @@ allprojects {
     version = "1.5.0-SNAPSHOT"
 
     repositories {
-        mavenLocal()
+        //mavenLocal()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         mavenCentral()
         google()
@@ -27,11 +27,11 @@ kotlin {
     val ktorVersion: String by extra { "2.0.3" }
     fun ktor(module: String) = "io.ktor:ktor-$module:$ktorVersion"
     val serializationVersion = "1.5.0"
-    val coroutineVersion = "1.7.0-Beta"
+    val coroutineVersion = "1.7.0"
 
     val commonMain by sourceSets.getting {
         dependencies {
-            api("fr.acinq.bitcoin:bitcoin-kmp:0.11.1") // when upgrading, keep secp256k1-kmp-jni-jvm in sync below
+            api("fr.acinq.bitcoin:bitcoin-kmp:0.12.0-SNAPSHOT") // when upgrading, keep secp256k1-kmp-jni-jvm in sync below
             api("org.kodein.log:kodein-log:0.13.0")
             api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
             api("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
@@ -61,7 +61,7 @@ kotlin {
             api(ktor("client-okhttp"))
             api(ktor("network"))
             api(ktor("network-tls"))
-            implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm:0.8.0")
+            implementation("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm:0.10.0-SNAPSHOT")
             implementation("org.slf4j:slf4j-api:1.7.36")
             api("org.xerial:sqlite-jdbc:3.32.3.2")
         }
