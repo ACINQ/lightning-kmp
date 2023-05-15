4798b41df3534a750c0474746de9da69bdfe4488
sstone <fabrice.drouin@acinq.fr>
2023-05-15 13:55:56 +0200

    Explain why the use of the local maven repo is commented out
    It sometimes leads to build errors that are hard to diagnose and reproduce on a different setup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,9 @@ allprojects {
     version = "1.5.0-SNAPSHOT"
 
     repositories {
-        //mavenLocal()
+        // using the local maven repository with Kotlin Multi Platform can lead to build errors that are hard to diagnose.
+        // uncomment this only if you need to experiment with snapshot dependencies that have not yet be published.
+        // mavenLocal()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         mavenCentral()
         google()
