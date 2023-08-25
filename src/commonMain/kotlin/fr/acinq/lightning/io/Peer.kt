bd41b0133f51d34060c61c4dfdee47154d787b90
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2023-08-25 17:14:04 +0200

    Manually start/stop swap-in wallet watcher (#518)
    The swap-in watcher is not automatically started by the Peer at initialization anymore. Library integrators must explicitly call `startWatchSwapInWallet()` after the peer is initialized.
    
    For the migration use case, it is useful to be able to wait for a certain number of UTXOs to be published before doing a swap-in.
    
    ---------
    
    Co-authored-by: Robbie Hanson <304604+robbiehanson@users.noreply.github.com>

--- src/commonMain/kotlin/fr/acinq/lightning/io/Peer.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/io/Peer.kt
@@ -185,6 +185,8 @@ class Peer(
     val swapInWallet = ElectrumMiniWallet(nodeParams.chainHash, watcher.client, scope, nodeParams.loggerFactory, name = "swap-in")
     val swapInAddress: String = nodeParams.keyManager.swapInOnChainWallet.address.also { swapInWallet.addAddress(it) }
 
+    private var swapInJob: Job? = null
+
     init {
         launch {
             watcher.client.notifications.filterIsInstance<HeaderSubscriptionResponse>()
@@ -227,13 +229,9 @@ class Peer(
             }
             logger.info { "restored ${channelIds.size} channels" }
             launch {
+                // the swap-in manager executes commands, but will not do anything until startWatchSwapInWallet() is called
                 val swapInManager = SwapInManager(bootChannels, logger)
                 processSwapInCommands(swapInManager)
-            }
-            launch {
-                // wait to have a swap-in feerate available
-                swapInFeeratesFlow.filterNotNull().first()
-                watchSwapInWallet()
             }
             launch {
                 // If we have some htlcs that have timed out, we may need to close channels to ensure we don't lose funds.
@@ -387,17 +385,32 @@ class Peer(
         listen() // This suspends until the coroutines is cancelled or the socket is closed
     }
 
-    private suspend fun watchSwapInWallet() {
-        swapInWallet.walletStateFlow.combine(currentTipFlow.filterNotNull()) { walletState, currentTip -> currentTip.first to walletState }
-            .filter { (_, walletState) -> walletState.consistent }
-            .collect { (currentBlockHeight, walletState) ->
-                // Local mutual close txs can be used as zero-conf inputs for swap-in
-                val mutualCloseTxs = channels.values
-                    .filterIsInstance<Closing>()
-                    .flatMap { state -> state.mutualClosePublished.map { closingTx -> closingTx.tx.txid } }
-                val trustedTxs = trustedSwapInTxs + mutualCloseTxs
-                swapInCommands.send(SwapInCommand.TrySwapIn(currentBlockHeight, walletState, walletParams.swapInConfirmations, trustedTxs))
-            }
+    /**
+     * This function needs to be called after [Peer] is initialized, to start watching the swap-in wallet
+     * and trigger swap-ins.
+     * Warning: not thread-safe!
+     */
+    suspend fun startWatchSwapInWallet() {
+        if (swapInJob != null) return
+        // wait to have a swap-in feerate available
+        swapInFeeratesFlow.filterNotNull().first()
+        swapInJob = launch {
+            swapInWallet.walletStateFlow.combine(currentTipFlow.filterNotNull()) { walletState, currentTip -> currentTip.first to walletState }
+                .filter { (_, walletState) -> walletState.consistent }
+                .collect { (currentBlockHeight, walletState) ->
+                    // Local mutual close txs can be used as zero-conf inputs for swap-in
+                    val mutualCloseTxs = channels.values
+                        .filterIsInstance<Closing>()
+                        .flatMap { state -> state.mutualClosePublished.map { closingTx -> closingTx.tx.txid } }
+                    val trustedTxs = trustedSwapInTxs + mutualCloseTxs
+                    swapInCommands.send(SwapInCommand.TrySwapIn(currentBlockHeight, walletState, walletParams.swapInConfirmations, trustedTxs))
+                }
+        }
+    }
+
+    suspend fun stopWatchSwapInWallet() {
+        swapInJob?.cancelAndJoin()
+        swapInJob = null
     }
 
     private suspend fun processSwapInCommands(swapInManager: SwapInManager) {
