4e5e9e3d5f3494c713a395c7f32450489a004eea
pm47 <pm.padiou@gmail.com>
2024-02-20 17:12:34 +0100

    set embedded web server log level to WARN

--- phoenixd/src/nativeMain/kotlin/fr/acinq/lightning/bin/Actuals.kt
+++ phoenixd/src/nativeMain/kotlin/fr/acinq/lightning/bin/Actuals.kt
@@ -3,14 +3,16 @@ package fr.acinq.lightning.bin
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import fr.acinq.phoenix.db.ChannelsDatabase
+import io.ktor.util.logging.*
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.toKString
 import okio.Path
 import okio.Path.Companion.toPath
 import platform.posix.getenv
+import platform.posix.setenv
 
 @OptIn(ExperimentalForeignApi::class)
-actual val homeDirectory: Path = getenv("HOME")?.toKString()!!.toPath()
+actual val homeDirectory: Path = setenv("KTOR_LOG_LEVEL", "WARN", 1).let { getenv("HOME")?.toKString()!!.toPath() }
 
 actual fun createAppDbDriver(dir: Path): SqlDriver {
     return NativeSqliteDriver(ChannelsDatabase.Schema, "phoenix.db",
