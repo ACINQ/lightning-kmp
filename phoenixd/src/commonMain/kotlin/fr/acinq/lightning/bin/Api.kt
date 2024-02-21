05f14ecfa0ecf46c4041f35b9a82f515daccf37e
pm47 <pm.padiou@gmail.com>
2024-02-21 14:25:02 +0100

    add bitcoin send api

--- phoenixd/src/commonMain/kotlin/fr/acinq/lightning/bin/Api.kt
+++ phoenixd/src/commonMain/kotlin/fr/acinq/lightning/bin/Api.kt
@@ -1,14 +1,22 @@
 package fr.acinq.lightning.bin
 
+import fr.acinq.bitcoin.Bitcoin
+import fr.acinq.bitcoin.Script
 import fr.acinq.bitcoin.utils.Either
+import fr.acinq.bitcoin.utils.toEither
 import fr.acinq.lightning.Lightning.randomBytes32
 import fr.acinq.lightning.NodeParams
 import fr.acinq.lightning.bin.json.Balance
 import fr.acinq.lightning.bin.json.PaymentReceived
+import fr.acinq.lightning.blockchain.fee.FeeratePerByte
+import fr.acinq.lightning.blockchain.fee.FeeratePerKw
+import fr.acinq.lightning.channel.ChannelCommand
 import fr.acinq.lightning.channel.states.ChannelStateWithCommitments
 import fr.acinq.lightning.io.Peer
+import fr.acinq.lightning.payment.PaymentRequest
 import fr.acinq.lightning.utils.sat
 import fr.acinq.lightning.utils.sum
+import fr.acinq.lightning.utils.toByteVector
 import fr.acinq.lightning.utils.toMilliSatoshi
 import io.ktor.serialization.kotlinx.*
 import io.ktor.serialization.kotlinx.json.*
@@ -62,6 +70,24 @@ class Api(private val nodeParams: NodeParams, private val peer: Peer) {
                 val description = formParameters["description"].toString()
                 val invoice = peer.createInvoice(randomBytes32(), amount, Either.Left(description))
                 call.respondText(invoice.write())
+            }
+            post("/bitcoin/send") {
+                val res = kotlin.runCatching {
+                    val formParameters = call.receiveParameters()
+                    val amount = (formParameters["amountSat"] ?: error("missing amountSat")).toLong().sat
+                    val address = formParameters["address"] ?: error("missing address")
+                    val scriptPubKey = Script.write(Bitcoin.addressToPublicKeyScript(nodeParams.chainHash, address).right ?: error("invalid address")).toByteVector()
+                    val feerate = FeeratePerKw(FeeratePerByte((formParameters["feerateSatByte"] ?: error("missing feerateSatByte")).toLong().sat))
+                    peer.spliceOut(amount, scriptPubKey, feerate)
+                }.toEither()
+                when (res) {
+                    is Either.Right -> when(val r = res.value) {
+                        is ChannelCommand.Commitment.Splice.Response.Created -> call.respondText(r.fundingTxId.toString())
+                        is ChannelCommand.Commitment.Splice.Response.Failure -> call.respondText(r.toString())
+                        else -> call.respondText("no channel available")
+                    }
+                    is Either.Left -> call.respondText(res.value.message.toString())
+                }
             }
             webSocket("/websocket") {
                 try {
