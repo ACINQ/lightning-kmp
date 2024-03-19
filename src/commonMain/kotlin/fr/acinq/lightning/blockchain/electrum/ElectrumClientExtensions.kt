df331409af2e0466f45f4360b5d705df5385f99f
pm47 <pm.padiou@gmail.com>
2024-03-19 18:23:30 +0100

    introduce IWatcher/IClient interfaces

--- src/commonMain/kotlin/fr/acinq/lightning/blockchain/electrum/ElectrumClientExtensions.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/blockchain/electrum/ElectrumClientExtensions.kt
@@ -1,16 +1,6 @@
 package fr.acinq.lightning.blockchain.electrum
 
-import fr.acinq.bitcoin.Satoshi
 import fr.acinq.bitcoin.Transaction
-import fr.acinq.bitcoin.TxId
-import fr.acinq.lightning.blockchain.fee.FeeratePerKw
-import fr.acinq.lightning.channel.Commitments
-import fr.acinq.lightning.channel.LocalFundingStatus
-import fr.acinq.lightning.logging.*
-import fr.acinq.lightning.transactions.Transactions
-import fr.acinq.lightning.utils.sat
-
-suspend fun IElectrumClient.getConfirmations(txId: TxId): Int? = getTx(txId)?.let { tx -> getConfirmations(tx) }
 
 /**
  * @return the number of confirmations, zero if the transaction is in the mempool, null if the transaction is not found
@@ -28,31 +18,3 @@ suspend fun IElectrumClient.getConfirmations(tx: Transaction): Int? {
     }
 }
 
-/**
- * @weight must be the total estimated weight of the splice tx, otherwise the feerate estimation will be wrong
- */
-suspend fun IElectrumClient.computeSpliceCpfpFeerate(commitments: Commitments, targetFeerate: FeeratePerKw, spliceWeight: Int, logger: MDCLogger): Pair<FeeratePerKw, Satoshi> {
-    val (parentsWeight, parentsFees) = commitments.all
-        .takeWhile { getConfirmations(it.fundingTxId).let { confirmations -> confirmations == null || confirmations == 0 } } // we check for null in case the tx has been evicted
-        .fold(Pair(0, 0.sat)) { (parentsWeight, parentsFees), commitment ->
-            val weight = when (commitment.localFundingStatus) {
-                // weight will be underestimated if the transaction is not fully signed
-                is LocalFundingStatus.UnconfirmedFundingTx -> commitment.localFundingStatus.signedTx?.weight() ?: commitment.localFundingStatus.sharedTx.tx.buildUnsignedTx().weight()
-                is LocalFundingStatus.ConfirmedFundingTx -> commitment.localFundingStatus.signedTx.weight()
-            }
-            Pair(parentsWeight + weight, parentsFees + commitment.localFundingStatus.fee)
-        }
-    val totalWeight = parentsWeight + spliceWeight
-    val totalFees = Transactions.weight2fee(targetFeerate, totalWeight)
-    val projectedFee = totalFees - parentsFees
-    val projectedFeerate = Transactions.fee2rate(projectedFee, spliceWeight)
-    // if ancestors have a higher feerate than target, min feerate could be negative
-    val actualFeerate = maxOf(projectedFeerate, targetFeerate)
-    val actualFee = Transactions.weight2fee(actualFeerate, spliceWeight)
-    logger.info { "targetFeerate=$targetFeerate spliceWeight=$spliceWeight" }
-    logger.info { "parentsWeight=$parentsWeight parentsFees=$parentsFees" }
-    logger.info { "totalWeight=$totalWeight totalFees=$totalFees" }
-    logger.info { "projectedFeerate=$projectedFeerate projectedFee=$projectedFee" }
-    logger.info { "actualFeerate=$actualFeerate actualFee=$actualFee" }
-    return Pair(actualFeerate, actualFee)
-}
