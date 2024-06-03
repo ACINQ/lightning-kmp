c8d1a0e9fbaaa7eb2438d7ce1ecf8acc6cd8ece0
pm47 <pm.padiou@gmail.com>
2024-06-03 16:43:35 +0200

    tests cleanup

--- src/commonTest/kotlin/fr/acinq/lightning/blockchain/mempool/MempoolSpaceWatcherTest.kt
+++ src/commonTest/kotlin/fr/acinq/lightning/blockchain/mempool/MempoolSpaceWatcherTest.kt
@@ -19,15 +19,6 @@ import kotlin.time.Duration.Companion.seconds
 class MempoolSpaceWatcherTest : LightningTestSuite() {
 
     @Test
-    fun `publish a transaction`() = runSuspendTest {
-        val client = MempoolSpaceClient(mempoolUrl = OfficialMempoolTestnet, testLoggerFactory)
-        val watcher = MempoolSpaceWatcher(client, scope = this, testLoggerFactory)
-        val tx =
-            Transaction.read("0200000000010146c398e70cceaf9d8f734e603bc53e4c4c0605ab46cb1b5807a62c90f5aed50d0100000000feffffff023c0fc10c010000001600145033f65b590f2065fe55414213f1d25ab20b6c4f487d1700000000001600144b812d5ef41fc433654d186463d41b458821ff740247304402202438dc18801919baa64eb18f7e925ab6acdedc3751ea58ea164a26723b79fd39022060b46c1d277714c640cdc8512c36c862ffc646e7ff62438ef5cc847a5990bbf801210247b49d9e6b0089a1663668829e573c629c936eb430c043af9634aa57cf97a33cbee81f00")
-        watcher.publish(tx)
-    }
-
-    @Test
     fun `watch-spent on a transaction`() = runSuspendTest {
         val client = MempoolSpaceClient(mempoolUrl = OfficialMempoolTestnet, testLoggerFactory)
         val watcher = MempoolSpaceWatcher(client, scope = this, testLoggerFactory)
@@ -44,6 +35,9 @@ class MempoolSpaceWatcherTest : LightningTestSuite() {
         watcher.watch(watch)
         val event = assertIs<WatchEventSpent>(notifications.first())
         assertEquals(TxId("5693d68997abfacb65cc7e6019e8ed2edb3f9f2260ae8d221e8726b8fe870ae0"), event.tx.txid)
+        // Right after checking whether the watched utxo is spent, a 2nd call is made by the watcher
+        // to find out whether the spending tx is confirmed, and the watch can be cleaned up. We give
+        // some time for that call to complete, in order to prevent a coroutine cancellation stack trace.
         delay(2.seconds)
     }
 
