35b70544bf683bc7fd14c0b817e5c4514e601ca0
pm47 <pm.padiou@gmail.com>
2024-06-14 15:56:43 +0200

    move bolt12 request failures to OfferManager

--- src/commonMain/kotlin/fr/acinq/lightning/payment/OfferManager.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/payment/OfferManager.kt
@@ -32,6 +32,16 @@ sealed class OnionMessageAction {
 
 private data class PendingInvoiceRequest(val payOffer: PayOffer, val request: OfferTypes.InvoiceRequest)
 
+/** Failures occurring when fetching an invoice to pay an offer */
+sealed class Bolt12InvoiceRequestFailure {
+    // @formatter:off
+    data class NoResponse(val request: OfferTypes.InvoiceRequest) : Bolt12InvoiceRequestFailure() { override fun toString(): String = "no response to the invoice request" }
+    data class MalformedResponse(val request: OfferTypes.InvoiceRequest, val failure: Bolt12Invoice.Companion.Bolt12ParsingResult.Failure.Malformed) : Bolt12InvoiceRequestFailure() { override fun toString(): String = "recipient returned an invalid response to the invoice request" }
+    data class ErrorFromRecipient(val request: OfferTypes.InvoiceRequest, val failure: Bolt12Invoice.Companion.Bolt12ParsingResult.Failure.RecipientError) : Bolt12InvoiceRequestFailure() { override fun toString(): String = "recipient responded to the invoice request with an error" }
+    data class InvoiceMismatch(val request: OfferTypes.InvoiceRequest, val reason: String) : Bolt12InvoiceRequestFailure() { override fun toString(): String = "recipient returned an invoice that does not match the request" }
+    // @formatter:on
+}
+
 class OfferManager(val nodeParams: NodeParams, val walletParams: WalletParams, val eventsFlow: MutableSharedFlow<PeerEvent>, private val logger: MDCLogger) {
     val remoteNodeId: PublicKey = walletParams.trampolineNode.id
     private val pendingInvoiceRequests: HashMap<ByteVector32, PendingInvoiceRequest> = HashMap()
