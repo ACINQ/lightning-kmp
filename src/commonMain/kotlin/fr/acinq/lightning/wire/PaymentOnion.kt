5c6fd67b27cab370e08949b1b8b114c1ffa76bb6
t-bast <bastien@acinq.fr>
2024-09-17 14:40:40 +0200

    Add support for trampoline blinded payments
    We add the ability to pay recipients that support trampoline *and*
    blinded paths. We include the blinded path data in the trampoline
    payloads for each node inside the blinded path. This doesn't reveal
    unnecessary information to the trampoline node: this is specified in
    details in https://github.com/lightning/bolts/pull/836.

--- src/commonMain/kotlin/fr/acinq/lightning/wire/PaymentOnion.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/wire/PaymentOnion.kt
@@ -399,6 +399,21 @@ object PaymentOnion {
                         else -> Either.Right(Blinded(records, blindedRecords))
                     }
                 }
+
+                fun create(amount: MilliSatoshi, expiry: CltvExpiry, encryptedData: ByteVector, blinding: PublicKey?): Blinded {
+                    val tlvs = TlvStream(
+                        setOfNotNull(
+                            OnionPaymentPayloadTlv.AmountToForward(amount),
+                            OnionPaymentPayloadTlv.OutgoingCltv(expiry),
+                            OnionPaymentPayloadTlv.TotalAmount(amount),
+                            OnionPaymentPayloadTlv.EncryptedRecipientData(encryptedData),
+                            blinding?.let { OnionPaymentPayloadTlv.BlindingPoint(it) },
+                        )
+                    )
+                    // We're creating a payload for an outgoing payment: we don't have access to the decrypted data, so we create an unused dummy one.
+                    val dummyPathId = ByteVector.fromHex("deadbeef")
+                    return Blinded(tlvs, RouteBlindingEncryptedData(TlvStream(RouteBlindingEncryptedDataTlv.PathId(dummyPathId))))
+                }
             }
         }
     }
