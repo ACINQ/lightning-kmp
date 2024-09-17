957f3f9e09b1af71b65872c4d7a3ffb4c36cde15
pm47 <pm.padiou@gmail.com>
2024-09-17 17:56:14 +0200

    fixup! upgrade to kotlin 2.0.20

--- src/iosMain/kotlin/fr/acinq/lightning/crypto/noise/Chacha20Poly1305CipherFunctions.kt
+++ src/iosMain/kotlin/fr/acinq/lightning/crypto/noise/Chacha20Poly1305CipherFunctions.kt
@@ -9,14 +9,14 @@ import kotlinx.cinterop.autoreleasepool
 
 
 actual object Chacha20Poly1305CipherFunctions : CipherFunctions {
-    override fun name() = "ChaChaPoly"
+    actual override fun name() = "ChaChaPoly"
 
     // as specified in BOLT #8
     fun nonce(n: Long): ByteArray = ByteArray(4) + ChaCha20Poly1305.write64(n)
 
     // Encrypts plaintext using the cipher key k of 32 bytes and an 8-byte unsigned integer nonce n which must be unique.
     @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
-    override fun encrypt(k: ByteArray, n: Long, ad: ByteArray, plaintext: ByteArray): ByteArray {
+    actual override fun encrypt(k: ByteArray, n: Long, ad: ByteArray, plaintext: ByteArray): ByteArray {
         autoreleasepool {
             val ciphertextAndMac = NativeChaChaPoly.chachapoly_encryptWithKey(
                 key = k.toNSData(),
@@ -32,7 +32,7 @@ actual object Chacha20Poly1305CipherFunctions : CipherFunctions {
     // Decrypts ciphertext using a cipher key k of 32 bytes, an 8-byte unsigned integer nonce n, and associated data ad.
     @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
     @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-    override fun decrypt(k: ByteArray, n: Long, ad: ByteArray, ciphertextAndMac: ByteArray): ByteArray {
+    actual override fun decrypt(k: ByteArray, n: Long, ad: ByteArray, ciphertextAndMac: ByteArray): ByteArray {
         autoreleasepool {
             val plaintext = NativeChaChaPoly.chachapoly_decryptWithKey(
                 key = k.toNSData(),
