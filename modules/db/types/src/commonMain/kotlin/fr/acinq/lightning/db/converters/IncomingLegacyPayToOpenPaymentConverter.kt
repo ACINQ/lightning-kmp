69335141a0e01c672c2cc635673b5c0e18686814
pm47 <pm.padiou@gmail.com>
2024-11-22 14:58:52 +0100

    fix IncomingLegacyPayToOpenPaymentConverter

--- modules/db/types/src/commonMain/kotlin/fr/acinq/lightning/db/converters/IncomingLegacyPayToOpenPaymentConverter.kt
+++ modules/db/types/src/commonMain/kotlin/fr/acinq/lightning/db/converters/IncomingLegacyPayToOpenPaymentConverter.kt
@@ -1,35 +1,65 @@
 package fr.acinq.lightning.db.converters
 
-import fr.acinq.lightning.db.InboundLiquidityOutgoingPayment
-import fr.acinq.lightning.db.converters.Converter
-import fr.acinq.lightning.db.types.OutgoingLiquidityPayment
-import fr.acinq.lightning.db.types.LiquidityAds.Purchase.Companion.toCoreType
-import fr.acinq.lightning.db.types.LiquidityAds.Purchase.Companion.toDbType
+import fr.acinq.lightning.db.types.IncomingLegacyPayToOpenPayment
 
-internal object IncomingLegacyPayToOpenPaymentConverter : Converter<InboundLiquidityOutgoingPayment, OutgoingLiquidityPayment> {
+internal object IncomingLegacyPayToOpenPaymentConverter : Converter<fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment, IncomingLegacyPayToOpenPayment> {
 
-    override fun toCoreType(o: OutgoingLiquidityPayment): fr.acinq.lightning.db.InboundLiquidityOutgoingPayment = when (o) {
-        is OutgoingLiquidityPayment.V0 -> fr.acinq.lightning.db.InboundLiquidityOutgoingPayment(
-            id = o.id,
-            channelId = o.channelId,
-            txId = o.txId,
-            localMiningFees = o.localMiningFees,
-            purchase = o.purchase.toCoreType(),
+    override fun toCoreType(o: IncomingLegacyPayToOpenPayment): fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment = when (o) {
+        is IncomingLegacyPayToOpenPayment.V0 -> fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment(
+            paymentPreimage = o.paymentPreimage,
+            origin = when (val origin = o.origin) {
+                is IncomingLegacyPayToOpenPayment.V0.Origin.Invoice -> fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Origin.Invoice(origin.paymentRequest)
+                is IncomingLegacyPayToOpenPayment.V0.Origin.Offer -> fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Origin.Offer(origin.metadata)
+            },
+            parts = o.parts.map { part ->
+                when (part) {
+                    is IncomingLegacyPayToOpenPayment.V0.Part.Lightning -> fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Part.Lightning(
+                        amountReceived = part.amountReceived,
+                        channelId = part.channelId,
+                        htlcId = part.htlcId
+                    )
+                    is IncomingLegacyPayToOpenPayment.V0.Part.OnChain -> fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Part.OnChain(
+                        amountReceived = part.amountReceived,
+                        serviceFee = part.serviceFee,
+                        miningFee = part.miningFee,
+                        channelId = part.channelId,
+                        txId = part.txId,
+                        confirmedAt = part.confirmedAt,
+                        lockedAt = part.lockedAt
+                    )
+                }
+            },
             createdAt = o.createdAt,
-            confirmedAt = o.confirmedAt,
-            lockedAt = o.lockedAt,
+            completedAt = o.completedAt
         )
     }
 
-    override fun toDbType(o: fr.acinq.lightning.db.InboundLiquidityOutgoingPayment): OutgoingLiquidityPayment =
-        OutgoingLiquidityPayment.V0(
-            id = o.id,
-            channelId = o.channelId,
-            txId = o.txId,
-            localMiningFees = o.localMiningFees,
-            purchase = o.purchase.toDbType(),
+    override fun toDbType(o: fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment): IncomingLegacyPayToOpenPayment =
+        IncomingLegacyPayToOpenPayment.V0(
+            paymentPreimage = o.paymentPreimage,
+            origin = when (val origin = o.origin) {
+                is fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Origin.Invoice -> IncomingLegacyPayToOpenPayment.V0.Origin.Invoice(origin.paymentRequest)
+                is fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Origin.Offer -> IncomingLegacyPayToOpenPayment.V0.Origin.Offer(origin.metadata)
+            },
+            parts = o.parts.map { part ->
+                when (part) {
+                    is fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Part.Lightning -> IncomingLegacyPayToOpenPayment.V0.Part.Lightning(
+                        amountReceived = part.amountReceived,
+                        channelId = part.channelId,
+                        htlcId = part.htlcId
+                    )
+                    is fr.acinq.lightning.db.LegacyPayToOpenIncomingPayment.Part.OnChain -> IncomingLegacyPayToOpenPayment.V0.Part.OnChain(
+                        amountReceived = part.amountReceived,
+                        serviceFee = part.serviceFee,
+                        miningFee = part.miningFee,
+                        channelId = part.channelId,
+                        txId = part.txId,
+                        confirmedAt = part.confirmedAt,
+                        lockedAt = part.lockedAt
+                    )
+                }
+            },
             createdAt = o.createdAt,
-            confirmedAt = o.confirmedAt,
-            lockedAt = o.lockedAt,
+            completedAt = o.completedAt
         )
 }
