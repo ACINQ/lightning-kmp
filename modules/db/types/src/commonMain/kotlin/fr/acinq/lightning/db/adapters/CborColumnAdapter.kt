16e2c486132c1c16bd62307bb64cf410c1683d89
pm47 <pm.padiou@gmail.com>
2024-12-05 15:29:07 +0100

    add cbor adapter

---
+++ modules/db/types/src/commonMain/kotlin/fr/acinq/lightning/db/adapters/CborColumnAdapter.kt
@@ -0,0 +1,34 @@
+package fr.acinq.lightning.db.adapters
+
+import app.cash.sqldelight.ColumnAdapter
+import fr.acinq.lightning.db.converters.Converter
+import fr.acinq.lightning.db.converters.IncomingPaymentConverter
+import fr.acinq.lightning.db.types.IncomingPayment
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.json.Json.Default.serializersModule
+import kotlinx.serialization.serializer
+
+/**
+ * Generic adapter that uses cbor encoding.
+ */
+@OptIn(ExperimentalSerializationApi::class)
+internal inline fun <C : Any, reified D : Any> cborColumnAdapter(converter: Converter<C, D>): ColumnAdapter<C, ByteArray> {
+    val serializer: KSerializer<D> = serializersModule.serializer()
+    val format = Cbor {
+
+    }
+    return object : ColumnAdapter<C, ByteArray> {
+
+        override fun decode(databaseValue: ByteArray): C {
+            return converter.toCoreType(format.decodeFromByteArray(serializer, databaseValue))
+        }
+
+        override fun encode(value: C): ByteArray {
+            return format.encodeToByteArray(serializer, converter.toDbType(value))
+        }
+    }
+}
+
+val IncomingPaymentCborAdapter = jsonColumnAdapter<fr.acinq.lightning.db.IncomingPayment, IncomingPayment>(IncomingPaymentConverter)
--- modules/db/types/src/commonMain/kotlin/fr/acinq/lightning/db/adapters/JsonColumnAdapter.kt
+++ modules/db/types/src/commonMain/kotlin/fr/acinq/lightning/db/adapters/JsonColumnAdapter.kt
@@ -2,9 +2,7 @@ package fr.acinq.lightning.db.adapters
 
 import app.cash.sqldelight.ColumnAdapter
 import fr.acinq.lightning.db.converters.Converter
-import fr.acinq.lightning.db.converters.IncomingLightningPaymentConverter
 import fr.acinq.lightning.db.converters.IncomingPaymentConverter
-import fr.acinq.lightning.db.types.IncomingLightningPayment
 import fr.acinq.lightning.db.types.IncomingPayment
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
@@ -16,21 +14,19 @@ import kotlinx.serialization.serializer
  */
 internal inline fun <C : Any, reified D : Any> jsonColumnAdapter(converter: Converter<C, D>): ColumnAdapter<C, String> {
     val serializer: KSerializer<D> = serializersModule.serializer()
-    val json = Json {
+    val format = Json {
         prettyPrint = true
     }
     return object : ColumnAdapter<C, String> {
 
         override fun decode(databaseValue: String): C {
-            return converter.toCoreType(json.decodeFromString(serializer, databaseValue))
+            return converter.toCoreType(format.decodeFromString(serializer, databaseValue))
         }
 
         override fun encode(value: C): String {
-            return json.encodeToString(serializer, converter.toDbType(value))
+            return format.encodeToString(serializer, converter.toDbType(value))
         }
     }
 }
 
-val IncomingLightningPaymentAdapter = jsonColumnAdapter<fr.acinq.lightning.db.LightningIncomingPayment, IncomingLightningPayment>(IncomingLightningPaymentConverter)
-
-val IncomingPaymentAdapter = jsonColumnAdapter<fr.acinq.lightning.db.IncomingPayment, IncomingPayment>(IncomingPaymentConverter)
+val IncomingPaymentJsonAdapter = jsonColumnAdapter<fr.acinq.lightning.db.IncomingPayment, IncomingPayment>(IncomingPaymentConverter)
