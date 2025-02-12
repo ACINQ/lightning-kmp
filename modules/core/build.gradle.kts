ca0e9c0faea12424a5f83a01f91f584223146475
pm47 <pm.padiou@gmail.com>
2025-02-12 11:13:22 +0100

    Revert "upgrade gradle to 8.10.2 and target java 21"
    This reverts commit 779fa88590c153d3fa1c982bc1e7eb10b24c445a.

--- modules/core/build.gradle.kts
+++ modules/core/build.gradle.kts
@@ -16,8 +16,9 @@ val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 kotlin {
     jvm {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_21)
+            jvmTarget.set(JvmTarget.JVM_1_8) // TODO: update this?
         }
     }
 
