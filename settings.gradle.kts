3b5d1490e9642776457c917e205d05fc509577df
Fabrice Drouin <sstone@users.noreply.github.com>
2025-07-08 11:16:41 +0200

    Use bitcoin-kmp 0.25.1 and set jdk 1.8 compiler option (#795)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
 }
 
@@ -10,9 +9,9 @@ dependencyResolutionManagement {
         // using the local maven repository with Kotlin Multi Platform can lead to build errors that are hard to diagnose.
         // uncomment this only if you need to experiment with snapshot dependencies that have not yet be published.
         // mavenLocal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
         mavenCentral()
         google()
+        maven("https://central.sonatype.com/repository/maven-snapshots")
     }
 }
 
