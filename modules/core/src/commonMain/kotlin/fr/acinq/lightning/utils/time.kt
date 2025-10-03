fa9dc06ecb6d1b8afc78d8a17f3600bd6bce019e
pm47 <pm47@users.noreply.github.com>
2025-10-03 11:14:39 +0200

    Upgrade kotlin to 2.2.0 (#812)
    * upgrade kotlin to 2.2.0
    
    Let's take advantage of the upcoming major release to upgrade kotlin and other dependencies.
    
    The main point of attention is a breaking change in `kotlinx-datetime`: see https://github.com/Kotlin/kotlinx-datetime?tab=readme-ov-file#deprecation-of-instant.
    
    * remove serialization-cbor dependency
    
    It is unused since #685.

--- modules/core/src/commonMain/kotlin/fr/acinq/lightning/utils/time.kt
+++ modules/core/src/commonMain/kotlin/fr/acinq/lightning/utils/time.kt
@@ -1,7 +1,9 @@
 package fr.acinq.lightning.utils
 
-import kotlinx.datetime.Clock
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 fun currentTimestampMillis(): Long = Clock.System.now().toEpochMilliseconds()
 
 fun currentTimestampSeconds(): Long = currentTimestampMillis() / 1000
